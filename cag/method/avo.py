0ff294a88b8ce5792ca5982877416a1117d34473
Joeri Hermans <joeri@joerihermans.com>
2018-10-08 13:33:40 +0200

    Add Adversarial Variational Optimization

--- cag/method/avo.py
+++ cag/method/avo.py
@@ -5,6 +5,22 @@ Adversarial Variational Optimization
 import cag.baseline
 import cag.method
 
+from cag.util import sample
+
+
+
+def r1(y_hat, x):
+    batch_size = x.size(0)
+    grad_y_hat = torch.autograd.grad(
+        outputs=y_hat.sum(),
+        inputs=x,
+        create_graph=True,
+        retain_graph=True,
+        only_inputs=True
+    )[0]
+    regularizer = grad_y_hat.pow(2).view(batch_size, -1).sum(1)
+
+    return regularizer
 
 
 class AdversarialVariationalOptimization(Method):
@@ -54,12 +70,50 @@ class AdversarialVariationalOptimization(Method):
         self._allocate_optimizers()
 
     def _update_critic(self, observations, thetas, x_thetas):
-        raise NotImplementedError
+        # Sample some observations.
+        x_real = sample(observations, self.batch_size)
+        x_fake = x_thetas
+        x_real.requires_grad = True
+        y_real = self.discriminator(x_real)
+        y_fake = self.discriminator(x_fake)
+        loss = (self._criterion(y_real, self._real) + self._criterion(y_fake, self._fake)) / 2.
+        loss = loss + self._r1 * r1(y_real, x_real).mean()
+        loss.backward()
+        self._o_discriminator.step()
 
     def _update_proposal(self, observations, thetas, x_thetas):
-        raise NotImplementedError
+        log_probabilities = self.proposal.log_prob(thetas)
+        gradients = []
+        for log_p in log_probabilities:
+            gradient = torch.autograd.grad(log_p, self.proposal.parameters(), create_graph=True)
+            gradients.append(gradient)
+        gradient_U = torch.zeros_like(gradients[0])
+        p_thetas = self._baseline.apply(gradients, x_thetas)
+        for index, gradient in enumerate(gradients):
+            p_theta = p_thetas[index]
+            for pg_index, pg in enumerate(gradient):
+                pg_theta = p_theta[pg_index]
+                gradient_U[pg_index] += -pg_theta * pg
+        gradient_U /= self.batch_size
+        for index, p in enumerate(self.proposal.parameters()):
+            p.grad = gradient_U[index].expand(p.size()).detach()
+        self._o_proposal.step()
+        self.proposal.fix()
+
+    def _sample(self):
+        thetas = self.proposal.sample(self.batch_size)
+        thetas, x_thetas = self.simulator(thetas)
+        self._num_simulations += self.batch_size
+
+    def step(self, x_o):
+        thetas, x_thetas = self._sample()
+        self._update_critic(x_o, thetas, x_thetas)
+        self._update_proposal(x_o, thetas, x_thetas)
 
     def infer(self, x_o, num_iterations=1000):
         self._reset()
 
-        raise NotImplementedError
+        for iteration in range(num_iterations):
+            self.step(x_o)
+
+        return self.proposal.clone()
