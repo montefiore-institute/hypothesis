9b1a6202a02f89bb6da9fe78d95bb1c4ccac6f5a
Joeri Hermans <joeri@joerihermans.com>
2018-10-10 13:31:27 +0200

    Add AVO for circle problem

--- cag/simulator.py
+++ cag/simulator.py
@@ -18,3 +18,17 @@ class Simulator(torch.nn.Module):
 
     def terminate(self):
         raise NotImplementedError
+
+
+class NoiseSimulator(torch.nn.Module):
+
+    def __init__(self, simulator, sigma=1.):
+        super(NoiseSimulator, self).__init__()
+        self._simulator = simulator
+        self._sigma = sigma
+
+    def forward(self, thetas):
+        thetas, x_thetas = self._simulator(thetas)
+        x_thetas = x_thetas + self._sigma * torch.rand_like(x_thetas)
+
+        return thetas, x_thetas
