4c96a239a05cabd9bb8d328ee787c73d6a7dabe5
Joeri Hermans <joeri@joerihermans.com>
2018-10-12 14:10:10 +0200

    Add CES

--- cag/inference/es.py
+++ cag/inference/es.py
@@ -16,19 +16,23 @@ class ClassifierEvolutionaryStrategy(Method):
     def __init__(self, simulator,
                  classifier,
                  num_particles=32,
+                 batch_size=32,
                  classifier_steps=2,
                  lr_classifier=0.0001,
+                 k=5,
                  r1_regularization=10.):
         super(ClassifierEvolutionaryStrategy, self).__init__(simulator)
         self.classifier = classifier
         self.num_particles = num_particles
+        self.batch_size = batch_size
         self.particles = None
+        self.k = k
         self._classifier_steps = classifier_steps
-        self._lr_classifier = _lr_classifier
+        self._lr_classifier = lr_classifier
         self._r1 = float(r1_regularization)
         self._criterion = torch.nn.BCELoss()
-        self._real = torch.zeros(self.num_particles, 1)
-        self._fake = torch.ones(self.num_particles, 1)
+        self._real = torch.zeros(self.batch_size, 1)
+        self._fake = torch.ones(self.batch_size, 1)
         self._o_classifier = None
         self._thetas_simulated = None
         self._x_thetas_simulated = None
@@ -58,8 +62,8 @@ class ClassifierEvolutionaryStrategy(Method):
                 self._x_thetas_simulated = torch.cat([x_t, x_thetas], dim=0)
 
     def _sample_simulated(self):
-        thetas = sample(self._thetas_simulated, self.num_particles)
-        x_thetas = sample(self._x_thetas_simulated, self.num_particles)
+        thetas = sample(self._thetas_simulated, self.batch_size)
+        x_thetas = sample(self._x_thetas_simulated, self.batch_size)
 
         return thetas, x_thetas
 
@@ -71,33 +75,43 @@ class ClassifierEvolutionaryStrategy(Method):
 
     def _optimize_classifier(self, x_o):
         _, x_fake = self._sample_simulated()
-        x_real = sample(x_o)
+        x_real = sample(x_o, self.batch_size)
         x_real.requires_grad = True
         y_real = self.classifier(x_real)
-        y_fake = self.classifier(y_fake)
-        loss = (self._criterion(y_real, self._real) + self._criterion(y_fake, self._fake)) / 2.
-        loss = loss + self._r1 * r1(y_real, x_real).mean()
+        y_fake = self.classifier(x_fake)
+        critic = self.classifier
+        loss = -(critic(x_real).log() + (1 - critic(x_fake)).log()).mean()
+        gradients = torch.autograd.grad(loss, critic.parameters(), create_graph=True)
+        gradient_norm = 0
+        for gradient in gradients:
+            gradient_norm = gradient_norm + (gradient ** 2).norm(p=1)
+        gradient_norm /= len(gradients)
+        loss = loss + gradient_norm
+        #loss = loss + self._r1 * r1(y_real, x_real).mean()
         self._o_classifier.zero_grad()
         loss.backward()
         self._o_classifier.step()
         x_real.requires_grad = False
 
-    def _update(self, k=5):
-        pi = self.classifier(self.particles)
-        top, top_indices = torch.topk(pi, k, dim=0, largest=True)
-        particles_hat = self.particles[top_indices]
+    def _update(self, x_o, x_thetas):
+        pi = self.classifier(x_thetas)
+        top, top_indices = torch.topk(pi, self.k, dim=0, largest=True)
+        particles_hat = self.particles[top_indices].squeeze()
         mean = particles_hat.mean(dim=0)
         sigma = particles_hat.std(dim=0)
-        new_particles = ((torch.randn(self.num_particles - k, self.particles.size(0)) * sigma) + mean).squeeze()
+        pi_hat = pi[top_indices].squeeze()
+        sigma_correction = pi_hat.mean() * torch.ones(self.particles.size(1)) * (self.classifier(sample(x_o, self.k)).mean() - pi_hat.mean())
+        sigma += sigma_correction
+        new_particles = (torch.randn(self.num_particles - self.k, self.particles.size(1)) * sigma) + mean
         self.particles = torch.cat([new_particles, particles_hat], dim=0).detach()
-        print(self.particles.mean(dim=0))
 
     def step(self, x_o):
         thetas, x_thetas = self.simulator(self.particles)
+        self.particles = thetas
         self._add_simulated(thetas, x_thetas)
         for training_iteration in range(self._classifier_steps):
             self._optimize_classifier(x_o)
-        self._update()
+        self._update(x_o, x_thetas)
 
     def infer(self, x_o, initializer, num_steps=1000):
         self._reset()
