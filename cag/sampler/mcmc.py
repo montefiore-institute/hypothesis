a118eabe61d49634f818dcd903776be4759b69ef
Joeri Hermans <joeri@joerihermans.com>
2018-10-15 15:41:15 +0200

    Add multiple transition distributions and Metroplis Hastings with symmetrical proposals

--- cag/sampler/mcmc.py
+++ cag/sampler/mcmc.py
@@ -12,14 +12,48 @@ from cag.sampler import Sampler
 class MetropolisHastings(Sampler):
 
     def __init__(self, simulator,
-                 transition,
-                 likelihood):
+                 likelihood,
+                 transition=None,
+                 warmup_steps=10):
         super(MetropolisHastings, self).__init__(simulator)
         self.transition = transition
         self.likelihood = likelihood
+        self._warmup = warmup_steps
 
-    def sample(self):
-        raise NotImplementedError
+    def _warmup(self, theta):
+        # Apply the burn-in / warm-up period.
+        for step in range(self._warmup):
+            theta = self.step(theta)
+
+    def step(self, x, p_x):
+        accepted = False
+
+        while not accepted:
+            x_next = self.transition.sample(x)
+            p_x_next = self.likelihood(x_next)
+            u = np.random.uniform()
+            if u <= p_x_next:
+                x = x_next
+                p_x = p_x_next
+                accepted = True
+
+        return x, p_x
+
+    def sample(self, initializer, num_samples):
+        samples = []
+
+        # Draw a random initial sample from the initializer.
+        x = initializer.sample().detach()
+        p_x = self.likelihood(x)
+        # Start the warmup period.
+        x, p_x = self._warmup(x)
+        samples.append(x)
+        # Start the sampling procedure.
+        for step in range(num_samples):
+            x, p_x = self.step(x, p_x)
+            samples.append(x)
+
+        return torch.cat(samples, dim=0)
 
 
 class ClassifierMetropolisHastings(Sampler):
--- cag/sampler/transition.py
+++ cag/sampler/transition.py
@@ -12,7 +12,7 @@ from torch.distributions.normal import Normal
 
 class TransitionDistribution:
 
-    def log_prob(self, thetas_next, thetas_current):
+    def log_prob(self, thetas_current, thetas_next):
         raise NotImplementedError
 
     def sample(self, thetas, samples=1):
@@ -25,7 +25,7 @@ class NormalTransitionDistribution(TransitionDistribution):
         super(NormalTransitionDistribution, self).__init__()
         self._sigma = sigma
 
-    def log_prob(self, thetas_next, thetas_current):
+    def log_prob(self, thetas_current, thetas_next):
         normal = Normal(thetas_current, self._sigma)
 
         return normal.log_prob(thetas_next)
@@ -41,10 +41,11 @@ class NormalTransitionDistribution(TransitionDistribution):
 class MultivariateNormalTransitionDistribution(TransitionDistribution):
 
     def __init__(self, sigma):
+        super(MultivariateNormalTransitionDistribution, self).__init__()
         self._dimensionality = sigma.size(0)
         self._sigma = sigma
 
-    def log_prob(self, thetas_next, thetas_current):
+    def log_prob(self, thetas_current, thetas_next):
         normal = MultivariateNormal(thetas_current, self._sigma)
 
         return normal.log_prob(thetas_next)
@@ -53,10 +54,37 @@ class MultivariateNormalTransitionDistribution(TransitionDistribution):
         x = []
 
         with torch.no_grad():
-            thetas = thetas.view(-1, 3)
+            thetas = thetas.view(-1, self._dimensionality)
             size = torch.Size([samples])
             for theta in thetas:
                 x_thetas = MultivariateNormal(theta.view(-1), self._sigma).sample(size).view(-1, self._dimensionality, samples)
+                x.append(x_thetas)
+            x = torch.cat(x, dim=0)
+
+        return x
+
+
+class UniformTransitionDistribution(TransitionDistribution):
+
+    def __init__(self, min_bound, max_bound):
+        super(UniformTransitionDistribution, self).__init__()
+        self._min_bound = torch.tensor(min_bound).float().view(-1, 1)
+        self._max_bound = torch.tensor(max_bound).float().view(-1, 1)
+        self._dimensionality = self._min_bound.size(0)
+
+    def log_prob(self, thetas_current, thetas_next):
+        uniform = Uniform(self._min_bound, self._max_bound)
+
+        return normal.log_prob(thetas_next)
+
+    def sample(self, thetas, samples=1):
+        x = []
+
+        with torch.no_grad():
+            thetas = thetas.view(-1, self._dimensionality)
+            size = torch.Size([samples])
+            for theta in thetas:
+                x_thetas = Uniform(self._min_bound, self._max_bound).sample(size).view(-1, self._dimensionality, samples)
                 x.append(x_thetas)
             x = torch.cat(x, dim=0)
 
