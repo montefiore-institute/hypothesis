ab6066394eb7f1bb81ea35f16f21b32b0acd333a
Joeri Hermans <joeri@joerihermans.com>
2018-10-19 16:18:11 +0200

    Add likelihood-free Metropolis-Hastings

--- cag/inference/mcmc.py
+++ cag/inference/mcmc.py
@@ -33,37 +33,20 @@ class MetropolisHastings(Method):
         raise NotImplementedError
 
 
-class ClassifierMetropolisHastings(Method):
+class LikelihoodFreeMetropolisHastings(Method):
 
     def __init__(self, simulator,
-                 discriminator,
+                 classifier,
                  transition,
                  warmup_steps=10,
                  simulations=10000):
-        super(ClassifierMetropolisHastings, self).__init__(simulator)
-        self.discriminator = discriminator
+        super(LikelihoodFreeMetropolisHastings, self).__init__(simulator)
+        self.classifier = classifier
         self.transition = transition
         self._warmup_steps = warmup_steps
         self._simulations = simulations
         self._epsilon = 10e-7
         self._o_discriminator = None
-
-    def _warmup(self, theta):
-        raise NotImplementedError
-
-    def _reset_discriminator(self):
-        # Reinitialize the weights of the discriminator.
-        with torch.no_grad():
-            for p in self.discriminator.parameters():
-                p.set_(torch.randn_like(p) * .25)
-        # Allocate a new optimizer for the discriminator.
-        self._o_discriminator = torch.optim.Adam(
-            self.discriminator.parameters()
-        )
-
-    def _train_classifier(self, theta_0, data_0, theta_1, data_1):
-        # Reset the current discriminator.
-        self._reset_discriminator()
 
     def _simulate(self, theta):
         theta = torch.cat([theta] * self._simulations, dim=0)
@@ -71,40 +54,44 @@ class ClassifierMetropolisHastings(Method):
 
         return x_theta
 
-    def _likelihood_ratio(self, x_o):
-        raise NotImplementedError
+    def _warmup(self, x_o, theta, x_theta):
+        for step in range(self._warmup_steps):
+            theta, x_theta = self.step(x_o, theta, x_theta)
 
-    def step(self, x_o, theta, data_theta):
+        return theta, x_theta
+
+    def step(self, theta, x_theta):
         accepted = False
 
-        while not accepted:
-            # Sample the next theta.
+        while not accpeted:
+            # Sample the next theta from the proposal.
             theta_next = self.transition.sample(theta)
-            data_theta_next = self._simulate(theta_next)
-            # Train the classifier to obtain the likelihood ratio.
-            self._train_classifier(theta, data_theta, theta_next, data_theta_next)
-            likelihood_ratio = self._likelihood_ratio(x_o)
+            x_theta_next = self._simulate(theta_next)
+            likelihood_ratio = self._likelihood_ratio(x_o, theta_next, x_theta_next, theta, x_theta)
             if not self.transition.is_symmetric():
-                p *= (self.transition.log_prob(theta_next, theta) / (self.transition.log_prob(theta, theta_next) + self._epsilon))
+                t_theta_next = self.transition.log_prob(theta_next, theta)
+                t_theta = self.transition.log_prob(theta, theta_next)
+                p *= (t_theta_next / (t_theta + self._epsilon))
             alpha = min([1, p])
             u = np.random.uniform()
             if u <= alpha:
                 theta = theta_next
-                accepted = True
+                accpeted = True
 
-        return theta, data_theta
+        return theta, x_theta
 
     def infer(self, x_o, initializer, num_samples):
         samples = []
 
         # Draw a random initial sample from the initializer.
         theta = initializer.sample().detach().view(-1)
-        data_theta = self._simulate(theta)
-        # TODO Implement warmup period.
+        x_theta = self._simulate(theta)
+        # MCMC burn-in.
+        theta, x_theta = self._warmup(x_o, theta, x_theta)
         samples.append(theta)
         # Start the sampling procedure.
         for step in range(num_samples - 1):
-            theta, data_theta = self.step(x_o, theta, data_theta)
+            theta, x_theta = self.step(x_o, theta, x_theta)
             samples.append(theta)
 
         return torch.cat(samples, dim=0)
