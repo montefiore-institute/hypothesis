231593a05725deea19949a06dc3d7c70a407ba6b
Joeri Hermans <joeri@joerihermans.com>
2018-10-23 12:58:15 +0200

    Add basic events

--- cag/engine/event.py
+++ cag/engine/event.py
@@ -1,3 +1,19 @@
 """
 Event definitions.
 """
+
+
+
+class Event:
+
+    def __init__(self):
+        self._next_identifier = 0
+        self.add_event("start")
+        self.add_event("terminate")
+
+    def add_event(self, identifier):
+        setattr(self, identifier, self._next_identifier)
+        self._next_identifier += 1
+
+
+event = Event()
