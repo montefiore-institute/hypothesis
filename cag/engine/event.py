6b9cec309183b2b314b525f4318d76a1c4223bd0
Joeri Hermans <joeri@joerihermans.com>
2018-10-23 13:27:09 +0200

    Add events

--- cag/engine/event.py
+++ cag/engine/event.py
@@ -9,7 +9,13 @@ class Event:
     def __init__(self):
         self._next_identifier = 0
         self.add_event("start")
+        self.add_event("start_iteration")
+        self.add_event("end_batch")
+        self.add_event("start_iteration")
+        self.add_event("end_epoch")
         self.add_event("terminate")
+        self.add_event("log")
+        self.add_event("loss")
 
     def add_event(self, identifier):
         setattr(self, identifier, self._next_identifier)
