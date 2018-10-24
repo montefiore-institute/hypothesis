dd16f0d373cf462373665e23bdd1bc1089de1259
Joeri Hermans <joeri@joerihermans.com>
2018-10-24 16:23:50 +0200

    Add Classifier MCMC

--- cag/inference/__init__.py
+++ cag/inference/__init__.py
@@ -7,3 +7,4 @@ from .epf import ElasticParticleFiltering
 from .es import ClassifierEvolutionaryStrategy
 from .mcmc import MetropolisHastings
 from .mcmc import LikelihoodFreeMetropolisHastings
+from .mcmc import ClassifierMetropolisHastings
--- cag/inference/mcmc.py
+++ cag/inference/mcmc.py
@@ -37,6 +37,77 @@ class MetropolisHastings(Method):
         raise NotImplementedError
 
 
+class ClassifierMetropolisHastings(Method):
+
+    def __init__(self, simulator,
+                 transition,
+                 classifier=torch.nn.BCELoss(),
+                 warmup_steps=10):
+        super(TestMetropolisHastings, self).__init__(simulator)
+        self.classifier = classifier
+        self.transition = transition
+        self._warmup_steps = warmup_steps
+        self._epsilon = 10e-7
+        self._criterion = criterion
+
+    def _warmup(self, x_o, theta):
+        for step in range(self._warmup_steps):
+            theta = self.step(x_o, theta)
+
+        return theta
+
+    def _create_test(self, theta, x_o):
+        with torch.no_grad():
+            thetas = torch.cat([theta.view(-1, 1)] * x_o.size(0), dim=0)
+            x_in = torch.cat([thetas, x_o], dim=1)
+
+        return x_in
+
+    def _likelihood_ratio(self, x_o, theta_next, theta):
+        x_next = torch.cat([x_o, ])
+        x_current = self._create_test(x_o, theta)
+        x_next = self._create_test(x_o, theta_next)
+        s_x_current = self.classifier(x_current).mean()
+        s_x_next = self.classifier(x_next).mean()
+        r_current = s_x_current / (1 - s_x_current)
+        r_next = s_x_next / (1 - s_x_next)
+        r = r_next / r_current
+
+        return r.item()
+
+    def step(self, x_o, theta):
+        # Sample the next theta from the proposal.
+        theta_next = self.transition.sample(theta)
+        p = self._likelihood_ratio(x_o, theta_next, theta)
+        if not self.transition.is_symmetric():
+            t_theta_next = self.transition.log_prob(theta_next, theta)
+            t_theta = self.transition.log_prob(theta, theta_next)
+            p *= (t_theta_next / (t_theta + self._epsilon))
+        alpha = min([1, p])
+        u = np.random.uniform()
+        if u <= alpha:
+            theta = theta_next
+
+        return theta
+
+    def infer(self, x_o, initializer, num_samples):
+        samples = []
+
+        # Draw a random initial sample from the initializer.
+        theta = initializer.sample().detach().view(-1)
+        x_theta = self._simulate(theta)
+        # MCMC burn-in.
+        theta = self._warmup(x_o, theta, x_theta)
+        samples.append(theta)
+        # Start the sampling procedure.
+        for step in range(num_samples - 1):
+            theta = self.step(x_o, theta)
+            samples.append(theta)
+
+        return torch.cat(samples, dim=0)
+
+
+
 class LikelihoodFreeMetropolisHastings(Method):
 
     def __init__(self, simulator,
@@ -83,31 +154,42 @@ class LikelihoodFreeMetropolisHastings(Method):
         real = torch.ones(self.batch_size, 1)
         fake = torch.zeros(self.batch_size, 1)
         # Training of density ratio of x_o and x_t_next.
+        # for batch_index in range(num_batches):
+        #     x_real = sample(x_o, self.batch_size)
+        #     x_fake = sample(x_t_next, self.batch_size)
+        #     y_real = self.classifier(x_real)
+        #     y_fake = self.classifier(x_fake)
+        #     loss = (self._criterion(y_real, real) + self._criterion(y_fake, fake)) / 2.
+        #     self._o_classifier.zero_grad()
+        #     loss.backward()
+        #     self._o_classifier.step()
+        # # Obtain the likelihood ratio.
+        # lr_a = (self.classifier(x_o) - .5).abs()
+        # self._reset_classifier()
+        # # Training of density ratio of x_o and x_t.
+        # for batch_index in range(num_batches):
+        #     x_real = sample(x_o, self.batch_size)
+        #     x_fake = sample(x_t, self.batch_size)
+        #     y_real = self.classifier(x_real)
+        #     y_fake = self.classifier(x_fake)
+        #     loss = (self._criterion(y_real, real) + self._criterion(y_fake, fake)) / 2.
+        #     self._o_classifier.zero_grad()
+        #     loss.backward()
+        #     self._o_classifier.step()
+        # # Obtain the likelihood ratio.
+        # lr_b = (self.classifier(x_o) - .5).abs()
+        # lr = (lr_b.mean() / (lr_a.mean() + self._epsilon)).item()
         for batch_index in range(num_batches):
-            x_real = sample(x_o, self.batch_size)
-            x_fake = sample(x_t_next, self.batch_size)
-            y_real = self.classifier(x_real)
-            y_fake = self.classifier(x_fake)
-            loss = (self._criterion(y_real, real) + self._criterion(y_fake, fake)) / 2.
-            self._o_classifier.zero_grad()
-            loss.backward()
-            self._o_classifier.step()
-        # Obtain the likelihood ratio.
-        lr_a = (self.classifier(x_o) - .5).abs()
-        self._reset_classifier()
-        # Training of density ratio of x_o and x_t.
-        for batch_index in range(num_batches):
-            x_real = sample(x_o, self.batch_size)
+            x_real = sample(x_t_next, self.batch_size)
             x_fake = sample(x_t, self.batch_size)
             y_real = self.classifier(x_real)
             y_fake = self.classifier(x_fake)
-            loss = (self._criterion(y_real, real) + self._criterion(y_fake, fake)) / 2.
+            loss = (self._criterion(y_real, real) + self._criterion(y_fake, fake)) / 2.)
             self._o_classifier.zero_grad()
             loss.backward()
             self._o_classifier.step()
-        # Obtain the likelihood ratio.
-        lr_b = (self.classifier(x_o) - .5).abs()
-        lr = (lr_b.mean() / (lr_a.mean() + self._epsilon)).item()
+        s_x = self.classifier(x_o).mean()
+        lr = s_x / (1 - s_x)
 
         return lr
 
