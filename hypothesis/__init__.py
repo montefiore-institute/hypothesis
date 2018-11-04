c0aa97d0e84f695860f55c70b4485e1c2524b0e8
Joeri Hermans <joeri@joerihermans.com>
2018-11-04 11:05:19 +0100

    Add module initialization

---
+++ hypothesis/__init__.py
@@ -0,0 +1,11 @@
+"""
+`hypothesis` is a python module for inference in settings were the likelihood is
+intractable or unatainable.
+"""
+
+__version__ = "0.0.1"
+__author__ = [
+    "Joeri Hermans",
+    "Volodimir Begy"
+]
+__email__ = "joeri.hermans@doct.uliege.be"
