fc114e194becffac56c4a83980a79d4def560f62
Joeri Hermans <joeri@joerihermans.com>
2018-11-04 13:35:44 +0100

    Add engine module

---
+++ hypothesis/engine/event.py
@@ -0,0 +1,28 @@
+"""
+Base event definitions.
+"""
+
+
+
+class Event:
+
+    def __init__(self):
+        self.add_event("start")
+        self.add_event("terminate")
+        self.add_event("batch_start")
+        self.add_event("batch_end")
+        self.add_event("iteration_start")
+        self.add_Event("iteration_end")
+        self.add_event("epoch_start")
+        self.add_event("epoch_end")
+        self.add_event("log")
+
+    def add_event(self, identifier):
+        if not self.has_event(identifier):
+            setattr(self, identifier, identifier);
+
+    def has_event(self, identifier):
+        return hasattr(self, identifier)
+
+
+event = Event()
