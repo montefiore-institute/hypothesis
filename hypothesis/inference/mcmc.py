147f584b67c6316a3948ca0d264249a1dc97e6f3
Joeri Hermans <joeri@joerihermans.com>
2018-11-06 17:16:06 +0100

    Add reworked likelihood free metropolis hastings

--- hypothesis/inference/mcmc.py
+++ hypothesis/inference/mcmc.py
@@ -8,6 +8,7 @@ import torch
 
 from hypothesis.inference import Method
 from hypothesis.inference import SimulatorMethod
+from hypothesis.util import sample
 
 
 
@@ -20,20 +21,83 @@ class LikelihoodFreeMetropolisHastings(SimulatorMethod):
     def __init__(self, simulator,
                  transition,
                  classifier_allocator,
-                 theta_ref):
+                 simulator_samples=50000,
+                 batch_size=512,
+                 epochs=10):
         super(LikelihoodFreeMetropolisHastings, self).__init__(simulator)
         self.transition = transition
         self.classifier_allocator = classifier_allocator
-        self.theta_ref = theta_ref
+        self._simulator_samples = simulator_samples
+        self._batch_size = batch_size
+        self._epochs = epochs
+
+    def _train_classifier(self, x_theta, x_theta_next):
+        iterations = int((self._simulator_samples * self._epochs) / self._batch_size)
+        classifier = self.classifier_allocator()
+        optimizer = torch.optim.Adam(classifier.parameters())
+        real = torch.ones(batch_size, 1)
+        fake = torch.zeros(batch_size, 1)
+        bce = torch.nn.BCELoss()
+        # Start the training procedures.
+        for iteration in range(iterations):
+            x_fake = sample(x_theta, self._batch_size)
+            x_real = sample(x_theta_next, self._batch_size)
+            y_real = classifier(x_real)
+            y_fake = classifier(x_fake)
+            loss = (bce(y_real, real) + bce(y_fake, fake)) / 2.
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+        return classifier
 
     def step(self, observations, theta):
-        raise NotImplementedError
+        theta = theta.unsqueeze(0)
+        theta_next = self.transition.sample(theta).squeeze().unsqueeze(0)
+        theta, x_theta = self.simulator(torch.cat([theta] * self._simulator_samples, dim=0))
+        theta_next, x_theta_next = self.simulator(torch.cat([theta] * self._simulator_samples, dim=0))
+        classifier = self._train_classifier(x_theta, x_theta_next)
+        s = classifier(observations).log().sum().exp()
+        lr = s / (1 - s + 10e-7)
+        if not self.transition.is_symmetric():
+            t_theta_next = self.transition.log_prob(theta, theta_next).exp()
+            t_theta = self.transition.log_prob(theta_next, theta).exp()
+            p *= (t_theta_next / (t_theta + 10e-7))
+        else:
+            p = 1
+        acceptance = min([1, lr.exp() * p])
+        u = np.random.uniform()
+        if u <= acceptance:
+            theta = theta_next
+
+        return theta, acceptance
 
     def run_chain(self, theta_0, observations, num_samples):
-        raise NotImplementedError
+        thetas = []
+        probabilities = []
+
+        for sample_index in range(num_samples):
+            theta_0, acceptance = self.step(observations, theta_0)
+            thetas.append(theta_0.squeeze())
+            probabilities.append(acceptance)
+
+        return thetas, probabilities
 
     def procedure(self, observations, **kwargs):
-        raise NotImplementedError
+        # Initialize the sampling procedure.
+        theta_0 = kwargs[self.KEY_INITIAL_THETA]
+        num_samples = int(kwargs[self.KEY_NUM_SAMPLES])
+        if self.KEY_BURN_IN_STEPS in kwargs.keys():
+            burnin_steps = int(kwargs[self.KEY_BURN_IN_STEPS])
+        else:
+            burnin_steps = 0
+        # Start the burnin procedure.
+        thetas, probabilities = self.run_chain(theta_0, observations, burnin_steps)
+        # Start sampling form the MH chain.
+        thetas, probabilities = self.run_chain(theta_0, observations, num_samples)
+
+        return torch.tensor(thetas), probabilities
+
 
 
 class MetropolisHastings(Method):
@@ -49,7 +113,7 @@ class MetropolisHastings(Method):
         self.transition = transition
 
     def step(self, observations, theta):
-        theta_next = self.transition.sample(theta)
+        theta_next = self.transition.sample(theta).squeeze()
         likelihood_current = self.log_likelihood(theta, observations)
         likelihood_next = self.log_likelihood(theta_next, observations)
         lr = likelihood_next - likelihood_current
