2cd87766feddabc563a0f1ce18d682f614918a3c
Joeri Hermans <joeri@joerihermans.com>
2018-11-06 21:24:29 +0100

    Update framework with new API

--- hypothesis/inference/mcmc.py
+++ hypothesis/inference/mcmc.py
@@ -6,11 +6,37 @@ import copy
 import numpy as np
 import torch
 
+from hypothesis.engine import event
 from hypothesis.inference import Method
 from hypothesis.inference import SimulatorMethod
 from hypothesis.util import sample
 from hypothesis.util import epsilon
 
+
+
+class Chain:
+
+    def __init__(self, chain, probabilities,
+                 burnin_chain=None, burnin_probabilities=None):
+        self._chain = chain
+        self._probabilities = probabilities
+        self._burnin_chain = burnin_chain
+        self._burnin_probabilities = burnin_probabilities
+
+    def has_burnin(self):
+        return self._burnin_chain or self._burnin_probabilities
+
+    def chain(self):
+        return self._chain
+
+    def probabilities(self):
+        return self._probabilities
+
+    def burnin_chain(self):
+        return self._burnin_chain
+
+    def burnin_probabilities(self):
+        return self._burnin_probabilities
 
 
 class LikelihoodFreeMetropolisHastings(SimulatorMethod):
@@ -22,15 +48,20 @@ class LikelihoodFreeMetropolisHastings(SimulatorMethod):
     def __init__(self, simulator,
                  transition,
                  classifier_allocator,
-                 simulator_samples=10000,
-                 batch_size=512,
-                 epochs=10):
+                 simulator_samples=20000,
+                 batch_size=128,
+                 epochs=50):
         super(LikelihoodFreeMetropolisHastings, self).__init__(simulator)
         self.transition = transition
         self.classifier_allocator = classifier_allocator
         self._simulator_samples = simulator_samples
         self._batch_size = batch_size
         self._epochs = epochs
+        # Add LFHM specific events.
+        event.add_event("lfhm_train_start")
+        event.add_event("lfhm_train_end")
+        event.add_event("lfhm_simulation_start")
+        event.add_event("lfhm_simulation_end")
 
     def _train_classifier(self, x_theta, x_theta_next):
         iterations = int((self._simulator_samples * self._epochs) / self._batch_size)
@@ -49,7 +80,6 @@ class LikelihoodFreeMetropolisHastings(SimulatorMethod):
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
-        print("Trained")
 
         return classifier
 
@@ -59,12 +89,12 @@ class LikelihoodFreeMetropolisHastings(SimulatorMethod):
             theta_next = self.transition.sample(theta).squeeze().unsqueeze(0)
             theta_in = torch.cat([theta] * self._simulator_samples, dim=0)
             theta_next_in = torch.cat([theta_next] * self._simulator_samples, dim=0)
-            theta, x_theta = self.simulator(theta_in)
-            theta_next, x_theta_next = self.simulator(theta_next_in)
+            _, x_theta = self.simulator(theta_in)
+            _, x_theta_next = self.simulator(theta_next_in)
         classifier = self._train_classifier(x_theta, x_theta_next)
         with torch.no_grad():
-            s = classifier(observations).log().sum().exp()
-            lr = s / (1 - s + epsilon)
+            s = classifier(observations)
+            lr = (s / (1 - s + epsilon)).log().sum()
             if not self.transition.is_symmetric():
                 t_theta_next = self.transition.log_prob(theta, theta_next).exp()
                 t_theta = self.transition.log_prob(theta_next, theta).exp()
@@ -90,19 +120,26 @@ class LikelihoodFreeMetropolisHastings(SimulatorMethod):
         return thetas, probabilities
 
     def procedure(self, observations, **kwargs):
+        burnin_thetas = None
+        burnin_probabilities = None
+
         # Initialize the sampling procedure.
         theta_0 = kwargs[self.KEY_INITIAL_THETA]
         num_samples = int(kwargs[self.KEY_NUM_SAMPLES])
         if self.KEY_BURN_IN_STEPS in kwargs.keys():
             burnin_steps = int(kwargs[self.KEY_BURN_IN_STEPS])
-        else:
-            burnin_steps = 0
-        # Start the burnin procedure.
-        thetas, probabilities = self.run_chain(theta_0, observations, burnin_steps)
+            if burnin_steps > 0:
+                # Start the burnin procedure.
+                burnin_thetas, burnin_probabilities = self.run_chain(theta_0, observations, burnin_steps)
+                # Take the last theta as the initial starting point.
+                theta_0 = thetas[-1]
         # Start sampling form the MH chain.
         thetas, probabilities = self.run_chain(theta_0, observations, num_samples)
+        chain = Chain(
+            thetas, probabilities,
+            burnin_thetas, burnin_probabilities)
 
-        return torch.tensor(thetas), probabilities
+        return chain
 
 
 
@@ -149,16 +186,23 @@ class MetropolisHastings(Method):
         return thetas, probabilities
 
     def procedure(self, observations, **kwargs):
+        burnin_thetas = None
+        burnin_probabilities = None
+
         # Initialize the sampling procedure.
         theta_0 = kwargs[self.KEY_INITIAL_THETA]
         num_samples = int(kwargs[self.KEY_NUM_SAMPLES])
         if self.KEY_BURN_IN_STEPS in kwargs.keys():
             burnin_steps = int(kwargs[self.KEY_BURN_IN_STEPS])
-        else:
-            burnin_steps = 0
-        # Start the burnin procedure.
-        thetas, probabilities = self.run_chain(theta_0, observations, burnin_steps)
+            if burnin_steps > 0:
+                # Start the burnin procedure.
+                thetas, probabilities = self.run_chain(theta_0, observations, burnin_steps)
+                # Take the last theta as the initial starting point.
+                theta_0 = thetas[-1]
         # Start sampling form the MH chain.
         thetas, probabilities = self.run_chain(theta_0, observations, num_samples)
+        chain = Chain(
+            thetas, probabilities,
+            burnin_thetas, burnin_probabilities)
 
-        return torch.tensor(thetas), probabilities
+        return chain
