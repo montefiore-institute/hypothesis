ffc202c906d943f3afd72bce2dbf8d73e7ca34b6
Joeri Hermans <joeri@joerihermans.com>
2018-11-06 14:09:40 +0100

    Update Metropolis Hastings MCMC

--- hypothesis/inference/__init__.py
+++ hypothesis/inference/__init__.py
@@ -4,3 +4,4 @@ from .base import Method
 from .base import SimulatorMethod
 
 from .avo import AdversarialVariationalOptimization
+from .mcmc import MetropolisHastings
--- hypothesis/inference/mcmc.py
+++ hypothesis/inference/mcmc.py
@@ -8,3 +8,28 @@ import torch
 
 from hypothesis.inference import Method
 from hypothesis.inference import SimulatorMethod
+
+
+
+class MetropolisHastings(Method):
+
+    KEY_INITIAL_THETA = "theta_0"
+    KEY_NUM_SAMPLES = "samples"
+    KEY_BURN_IN_STEPS = "burnin_steps"
+
+    def __init__(self, log_likelihood,
+                 transition):
+        self.log_likelihood = log_likelihood
+        self.transition = transition
+
+    def step(self, observations, theta):
+        raise NotImplementedError
+
+    def procedure(self, observations, **kwargs):
+        # Initialize the sampling procedure.
+        theta_0 = kwargs[self.KEY_INITIAL_THETA]
+        num_samples = int(kwargs[self.KEY_NUM_SAMPLES])
+        if self.KEY_BURN_IN_STEPS in kwargs.keys():
+            burnin_steps = int(kwargs[self.KEY_BURN_IN_STEPS])
+        else:
+            burnin_steps = 0
