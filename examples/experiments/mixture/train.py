811eac3dd5f220a0f785a7d73aec4a77272dd4ad
VolodimirBegy <begy.volodimir@gmail.com>
2018-12-10 19:02:22 +0100

    experiments

---
+++ examples/experiments/mixture/train.py
@@ -0,0 +1,137 @@
+"""
+Trains a parameterized classifier to use with approximate likelihood-ratios.
+"""
+
+import argparse
+import torch
+import numpy as np
+import os
+
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+from hypothesis.distribution import MixtureOfNormals
+from torch.distributions.normal import Normal
+from torch.distributions.uniform import Uniform
+from tqdm import tqdm
+
+
+
+def main(arguments):
+    # Data-source preperation.
+    modes, mixing_coefficients = [-5., 0., 5.], [0.4, 0.2, 0.4]
+    if arguments.modes is not None:
+        modes = arguments.modes.split(",")
+        modes = [float(x) for x in modes]
+    if arguments.mixing_coefficients is not None:
+        mixing_coefficients = arguments.mixing_coefficients.split(",")
+        mixing_coefficients = [float(x) for x in mixing_coefficients]
+    dataset = SimulationDataset(2 * arguments.size, modes=modes,
+                                mixing_coefficients=mixing_coefficients,
+                                upper=arguments.upper, lower=arguments.lower)
+    # Training preperation.
+    ones = torch.ones(arguments.batch_size, 1)
+    zeros = torch.zeros(arguments.batch_size, 1)
+    criterion = torch.nn.BCELoss()
+    classifier = allocate_classifier(arguments)
+    optimizer = torch.optim.Adam(classifier.parameters(), lr=arguments.lr)
+    iterations = int(arguments.size / arguments.batch_size)
+    # Training procedure.
+    for epoch in tqdm(range(arguments.epochs)):
+        loader = iter(DataLoader(dataset, num_workers=arguments.workers, batch_size = arguments.batch_size))
+        for iteration in tqdm(range(iterations)):
+            theta, x_theta = next(loader)
+            _, x_theta_hat = next(loader)
+            x = torch.cat([theta, x_theta], dim=1).detach()
+            x_hat = torch.cat([theta, x_theta_hat], dim=1).detach()
+            y = classifier(x)
+            y_hat = classifier(x_hat)
+            loss = criterion(y, zeros) + criterion(y_hat, ones)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+        # Save the model at the current epoch.
+        save_model(arguments, classifier, name="classifier_" + str(epoch))
+    # Save the final model.
+    save_model(arguments, classifier, name="classifier_final")
+
+
+def save_model(arguments, model, name):
+    # Check if an output directory has been specified.
+    if arguments.out is not None:
+        models_directory = arguments.out + '/'
+    else:
+        models_directory = "models/"
+    if not os.path.exists(models_directory):
+        os.makedirs(models_directory)
+    path = models_directory + name + ".th"
+    torch.save(model, path)
+
+
+def allocate_classifier(arguments):
+    modules = []
+
+    hidden = arguments.hidden
+    # Add initial layer.
+    modules.append(torch.nn.Linear(2, hidden))
+    modules.append(torch.nn.LeakyReLU())
+    # Add hidden layers.
+    for i in range(arguments.layers):
+        modules.append(torch.nn.Linear(hidden, hidden))
+        modules.append(torch.nn.LeakyReLU())
+    # Add final layers.
+    modules.append(torch.nn.Linear(arguments.hidden, 1))
+    modules.append(torch.nn.Sigmoid())
+
+    return torch.nn.Sequential(*modules)
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser("Likelihood-free Posterior Sampling. Demonstration 1 - Training.")
+    parser.add_argument("--lr", type=float, default=0.00001, help="Learning-rate.")
+    parser.add_argument("--batch-size", type=int, default=256, help="Batch-size.")
+    parser.add_argument("--upper", type=float, default=5, help="Upper-limit of the parameter space.")
+    parser.add_argument("--lower", type=float, default=-5, help="Lower-limit of the parameter space.")
+    parser.add_argument("--epochs", type=int, default=100, help="Number of data iterations.")
+    parser.add_argument("--size", type=int, default=1000000, help="Number of samples in a single dataset.")
+    parser.add_argument("--hidden", type=int, default=256, help="Number of hidden units.")
+    parser.add_argument("--layers", type=int, default=3, help="Number of hidden layers.")
+    parser.add_argument("--workers", type=int, default=0, help="Number of asynchronous data loaders.")
+    parser.add_argument("--out", type=str, default=None, help="Directory to store the models.")
+    parser.add_argument("--modes", type=str, default=None, help="Modes of the mixture.")
+    parser.add_argument("--mixing-coefficients", type=str, default=None, help="Mixing coefficients for the modes.")
+    arguments, _ = parser.parse_known_args()
+
+    return arguments
+
+
+class SimulationDataset(Dataset):
+
+    def __init__(self, size, modes, mixing_coefficients,
+                lower=-5., upper=5.):
+        super(SimulationDataset, self).__init__()
+        self.size = size
+        self.uniform = Uniform(lower, upper)
+        self.modes = modes
+        self.mixing_coefficients = mixing_coefficients
+
+    def __getitem__(self, index):
+        return self.sample()
+
+    def __len__(self):
+        return self.size
+
+    def sample(self):
+        theta = self.uniform.sample().view(-1)
+
+        components = []
+        for x in self.modes:
+            components.append(Normal(x, theta))
+
+        x_theta = MixtureOfNormals(components, self.mixing_coefficients).sample().view(-1)
+
+        return theta, x_theta
+
+
+if __name__ == "__main__":
+    arguments = parse_arguments()
+    main(arguments)
--- hypothesis/distribution/MixtureOfNormals.py
+++ hypothesis/distribution/MixtureOfNormals.py
@@ -1,3 +1,5 @@
+from torch.distributions.uniform import Uniform
+
 class MixtureOfNormals:
 
     def __init__(self, normals, mixing_coefficients=None):
@@ -6,6 +8,11 @@ class MixtureOfNormals:
             self.mixing_coefficients = [1.0/len(normals)]*len(normals)
         else:
             self.mixing_coefficients = mixing_coefficients
+
+        self.component_thresholds = [0]
+        for x in self.mixing_coefficients[:-1]:
+                self.component_thresholds.append(self.component_thresholds[-1] + x)
+        self.component_thresholds.reverse()
 
     def prob(self, x):
         prob = 0
@@ -18,3 +25,15 @@ class MixtureOfNormals:
 
     def log_prob(self, x):
         return self.prob(x).log()
+
+    def sample(self):
+        threshold = Uniform(0, 1).sample()
+
+        component_index = 0
+
+        for i in range(len(self.component_thresholds)):
+            if(threshold >= self.component_thresholds[i]):
+                component_index = len(self.normals) - 1 - i
+                break
+
+        return self.normals[component_index].sample()
