79d2472b1b364abbb08399dece5d5f7c04dff200
Joeri Hermans <joeri@joerihermans.com>
2018-12-13 14:05:48 +0100

    Add simulation dataset with buffering support

--- hypothesis/io/dataset.py
+++ hypothesis/io/dataset.py
@@ -3,6 +3,7 @@ Datasets API.
 """
 
 import numpy as np
+import os
 import torch
 
 from torch.utils.data import Dataset
@@ -71,13 +72,44 @@ class SimulationDataset(HypothesisDataset):
 
     def __init__(self, path):
         super(SimulationDataset, self).__init__()
-        raise NotImplementedError
+        self.base = path
+        self.base_x = path + "/x/"
+        self.base_theta = path + "/theta/"
+        self.num_files = len([x for x in os.listdir(self.base_theta) if os.path.isfile(self.base_theta + x)])
+        self.buffer_block = 0
+        self.buffer_theta, self.buffer_x = self.load_block(self.buffer_block)
+        self.block_observations = len(self.buffer_theta)
+        self.size = self.block_observations * self.num_files
+
+    def load_block(self, index):
+        # Load the thetas.
+        index = str(index)
+        with np.load(self.base_theta + index + ".npz") as theta_data:
+            thetas = theta_data["arr_0"]
+        # Load the observations.
+        with np.load(self.base_x + index + ".npz") as x_data:
+            xs = x_data["arr_0"]
+
+        return thetas, xs
 
     def length(self):
         return self.size
 
     def sample(self):
-        raise NotImplementedError
+        u = np.random.randint(0, self.size)
+        return self.__getitem__(u)
 
     def __getitem__(self, index):
-        raise NotImplementedError
+        # Check if the block is buffered.
+        block_index = int(index / self.block_observations)
+        if block_index != self.buffer_block:
+            b_theta, b_x = self.load_block(block_index)
+            self.buffer_theta = b_theta
+            self.buffer_x = b_x
+            self.buffer_block = block_index
+        # Load the data from the buffer.
+        data_index = index % self.block_observations
+        theta = self.buffer_theta[data_index]
+        x = self.buffer_x[data_index]
+
+        return theta, x
