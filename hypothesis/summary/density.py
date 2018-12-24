f902295a7e9786f6faf622ca674ebc31bd34bc90
Joeri Hermans <joeri@joerihermans.com>
2018-12-24 10:20:41 +0100

    Add automatic bandwidth estimation with Scott's factor

--- hypothesis/summary/density.py
+++ hypothesis/summary/density.py
@@ -15,14 +15,11 @@ class Density:
     def log_prob(self, x):
         raise NotImplementedError
 
-    def sample(self, num_samples):
-        raise NotImplementedError
-
 
 
 class KernelDensity(Density):
 
-    def __init__(self, samples, bandwidth=0.05, kernel="normal"):
+    def __init__(self, samples, bandwidth="auto", kernel=kde_normal):
         super(KernelDensity, self).__init__()
         if bandwidth is "auto":
             self.bandwidth = kde_estimate_bandwidth(samples)
@@ -37,19 +34,40 @@ class KernelDensity(Density):
     def log_prob(self, x):
         raise NotImplementedError
 
-    def sample(self, num_samples):
-        raise NotImplementedError
+
+
+def kde_estimate_bandwidth(samples, method="scott"):
+    methods = {
+        "scott": kde_estimate_bandwidth_scott}
+
+    return methods[method](samples)
 
 
 
-def kde_estimate_bandwidth(samples):
-    raise NotImplementedError
+def kde_estimate_bandwidth_scott(samples):
+    d = samples.shape[1]
+    n = samples.shape[0]
+
+    return n ** (-1./ (d + 4))
+
 
 
 def kde_normal(samples, x, bandwidth):
     with torch.no_grad():
-        n = samples.dim(0)
-        k_h = ((samples - x) / bandwidth).sum(dim=0)
-        p_x = (1 / (n * bandwidth)) * k_h
+        normal = Normal(x, bandwidth)
+        n = samples.shape[0]
+        k_h = normal.log_prob(samples - x).exp().sum(dim=0)
+        p_x = (1 / n) * k_h
 
-    return p_x
+    return p_x.item()
+
+
+def kde_multivariate_normal(samples, x, bandwidth):
+    with torch.no_grad():
+        x = torch.tensor(x).float().view(1, -1)
+        d = x.shape[1]
+        normal = MultivariateNormal(x, bandwidth * torch.eye(d))
+        k_h = normal.log_prob(samples - x).exp().sum(dim=0)
+        p_x = (1 / n) * k_h
+
+    return p_x.squeeze()
