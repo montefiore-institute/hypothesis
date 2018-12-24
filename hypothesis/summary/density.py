ca650d4436c5b0c4d289171e011df8663ed0ac17
Joeri Hermans <joeri@joerihermans.com>
2018-12-24 10:44:23 +0100

    Update kernel density estimation

--- hypothesis/summary/density.py
+++ hypothesis/summary/density.py
@@ -17,25 +17,6 @@ class Density:
 
 
 
-class KernelDensity(Density):
-
-    def __init__(self, samples, bandwidth="auto", kernel=kde_normal):
-        super(KernelDensity, self).__init__()
-        if bandwidth is "auto":
-            self.bandwidth = kde_estimate_bandwidth(samples)
-        else:
-            self.bandwidth = bandwidth
-        self.kernel = kernel
-        self._samples = torch.cat([samples], dim=1)
-
-    def samples(self):
-        return self._samples
-
-    def log_prob(self, x):
-        raise NotImplementedError
-
-
-
 def kde_estimate_bandwidth(samples, method="scott"):
     methods = {
         "scott": kde_estimate_bandwidth_scott}
@@ -54,20 +35,30 @@ def kde_estimate_bandwidth_scott(samples):
 
 def kde_normal(samples, x, bandwidth):
     with torch.no_grad():
-        normal = Normal(x, bandwidth)
+        x = torch.tensor(x).float().view(-1)
         n = samples.shape[0]
-        k_h = normal.log_prob(samples - x).exp().sum(dim=0)
-        p_x = (1 / n) * k_h
-
-    return p_x.item()
-
-
-def kde_multivariate_normal(samples, x, bandwidth):
-    with torch.no_grad():
-        x = torch.tensor(x).float().view(1, -1)
-        d = x.shape[1]
+        d = x.shape[0]
         normal = MultivariateNormal(x, bandwidth * torch.eye(d))
-        k_h = normal.log_prob(samples - x).exp().sum(dim=0)
+        k_h = normal.log_prob(samples - x).sum(dim=0)
         p_x = (1 / n) * k_h
 
     return p_x.squeeze()
+
+
+
+class KernelDensity(Density):
+
+    def __init__(self, samples, bandwidth="auto", kernel=kde_normal):
+        super(KernelDensity, self).__init__()
+        if bandwidth is "auto":
+            self.bandwidth = kde_estimate_bandwidth(samples)
+        else:
+            self.bandwidth = bandwidth
+        self.kernel = kernel
+        self._samples = torch.cat([samples], dim=1)
+
+    def samples(self):
+        return self._samples
+
+    def log_prob(self, x):
+        return self.kernel(self._samples, x, self.bandwidth)
