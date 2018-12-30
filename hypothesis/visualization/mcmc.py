1166bb83d649955f55bdf0172a49bf998300b253
Joeri Hermans <joeri@joerihermans.com>
2018-12-30 14:06:47 +0100

    Update MCMC visualizations

--- hypothesis/visualization/mcmc.py
+++ hypothesis/visualization/mcmc.py
@@ -11,13 +11,34 @@ from hypothesis.util.common import parse_argument
 
 
 # Constants
-KEY_RADIUS = "radius"
+KEY_BINS = "bins"
 KEY_INTERVAL = "interval"
 KEY_MAX_LAG = "max_lag"
+KEY_PARAMETER_INDEX = "parameter_index"
+KEY_RADIUS = "radius"
+KEY_SHOW_MEAN = "show_mean"
+KEY_TRUTH = "truth"
+
+
+def plot_density(result, **kwargs):
+    # Fetch the specified arguments, or their defaults.
+    bins = get_argument(**kwargs, key=KEY_BINS, default=25, type=int)
+    parameter_index = get_argument(**kwargs, key=KEY_PARAMETER_INDEX, default=0, type=int)
+    show_mean = get_argument(**kwargs, key=KEY_SHOW_MEAN, default=False, type=bool)
+    truth = get_argument(**kwargs, key=KEY_TRUTH, default=None, type=float)
+    chain = result.chain(parameter_index=parameter_index)
+    weights = [1 / result.size()] * result.size()
+    plt.hist(chain.numpy(), bins=bins, weights=weights)
+    plt.grid(True, alpha=.75)
+    plt.minorticks_on()
+    if truth:
+        plt.axvline(truth, c='r', lw=2, linestyle="--",)
+    if show_mean:
+        plt.axvline(result.mean(parameter_index=parameter_index), c='y', lw=2, linestyle="--", alpha=.75)
 
 
 def plot_autocorrelation(result, **kwargs):
-    # Set the default arguments.
+    # Fetch the specified arguments, or their defaults.
     radius = get_argument(**kwargs, key=KEY_RADIUS, default=1.1, type=float)
     max_lag = get_argument(**kwargs, key=KEY_MAX_LAG, default=None, type=int)
     interval = get_argument(**kwargs, key=KEY_INTERVAL, default=5, type=int)
