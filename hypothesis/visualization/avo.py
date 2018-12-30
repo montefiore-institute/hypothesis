f649da08546d6c670598a96852bfe096a89af242
Joeri Hermans <joeri@joerihermans.com>
2018-12-30 14:20:12 +0100

    Update AVO visualizations

--- hypothesis/visualization/avo.py
+++ hypothesis/visualization/avo.py
@@ -2,15 +2,20 @@
 Visualization mechanisms for Adversarial Variational Optimization.
 """
 
-import matplotlib.mlab as mlab
 import matplotlib.pyplot as plt
 import numpy as np
+import scipy.stats
 import torch
+
+from hypothesis.util.common import parse_argument
 
 
 # Constants
-KEY_TRUTH = "truth"
+KEY_LABELX = "labelx"
+KEY_LABELY = "labely"
+KEY_SHOW_LABELS = "show_labels"
 KEY_SHOW_MEAN = "show_mean"
+KEY_TRUTH = "truth"
 
 
 def plot_proposal(proposal, **kwargs):
@@ -19,7 +24,7 @@ def plot_proposal(proposal, **kwargs):
         "NormalProposal": plot_normal_proposal,
         "MultivariateNormalProposal": plot_multivariate_normal_proposal}
     # Check if a method is available.
-    key = proposal.__name__
+    key = type(proposal).__name__
     if key not in methods.keys():
         raise NotImplementedError("Proposal visualization not implemented for " + key)
     methods[key](proposal, **kwargs)
@@ -30,19 +35,22 @@ def plot_normal_proposal(proposal, **kwargs):
     mean = proposal.mu.item()
     sigma = proposal.sigma.item()
     # Check if the truth has been specified.
-    if KEY_TRUTH in kwargs.keys():
-        truth = float(kwargs[KEY_TRUTH])
+    truth = parse_argument(kwargs, key=KEY_TRUTH, default=None, type=float)
+    show_mean = parse_argument(kwargs, key=KEY_SHOW_MEAN, default=False, type=bool)
+    show_labels = parse_argument(kwargs, key=KEY_SHOW_LABELS, default=True, type=bool)
+    labelx = parse_argument(kwargs, key=KEY_LABELX, default=r"$\theta$", type=str)
+    labely = parse_argument(kwargs, key=KEY_LABELY, default=r"$q(\theta)$", type=str)
+    if truth:
         plt.axvline(truth, c="red", lw=2, label=r"$\theta^*$")
-    # Check if the mean of the proposal needs to be plotted.
-    if KEY_SHOW_MEAN in kwargs.keys():
-        show_mean = bool(kwargs[KEY_SHOW_MEAN])
-        if show_mean:
-            plt.axvline(mean, c="gray", lw=2, style="--", alpha=.75, label="Proposal mean")
+    if show_mean:
+        plt.axvline(mean, c="gray", lw=2, linestyle="--", alpha=.75)
     # Plot the proposal.
     x = np.linspace(mean - 10 * sigma, mean + 10 * sigma, 5000)
-    plt.plot(x, mlab.normpdf(x, mean, sigma), label=r"$q({\theta}|{\psi})\ \gamma = 0$")
+    plt.plot(x, scipy.stats.norm.pdf(x, mean, sigma))
     plt.xlim([mean - 6 * sigma, mean + 6 * sigma])
-    plt.legend()
+    if show_labels:
+        plt.ylabel(labely)
+        plt.xlabel(labelx)
 
 
 def plot_multivariate_normal_proposal(proposal, **kwargs):
