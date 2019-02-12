5f46fb19857284b6e909ca5fe4e342b034d7bb13
Joeri Hermans <joeri@joerihermans.com>
2019-02-12 15:34:49 +0100

    Update HDF5 dataset

--- hypothesis/io/dataset.py
+++ hypothesis/io/dataset.py
@@ -1,4 +1,5 @@
 import glob
+import h5py
 import hypothesis
 import numpy as np
 import os
@@ -50,35 +51,13 @@ class GeneratorDataset(Dataset):
         return self.size
 
 
-class HDF5File:
-    def __init__(self, path):
-        self.fd = os.open(path, os.O_RDONLY)
-        self.pos = 0
-
-    def seek(self, pos, whence=0):
-        if whence == 0:
-            self.pos = pos
-        elif whence == 1:
-            self.pos += pos
-        else:
-            self.pos = os.lseek(self.fd, pos, whence)
-        return self.pos
-
-    def tell(self):
-        return self.pos
-
-    def read(self, size):
-        b = os.pread(self.fd, size, self.pos)
-        self.pos += len(b)
-        return b
-
-
 class HDF5SimulationDataset(Dataset):
     r"""TODO"""
 
     def __init__(self, path):
+        super(HDF5SimulationDataset, self).__init__()
         self.path = path
-        self.f = h5py.File(HDF5File(self.path), 'r')
+        self.f = h5py.File(self.path, 'r')
         self.dataset_thetas = self.f["thetas"]
         self.dataset_x_thetas = self.f["x_thetas"]
 
