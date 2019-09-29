77f17bd3f9dcb9a6eb67386d2b80e5b24903c029
Joeri Hermans <joeri@joerihermans.com>
2019-09-29 19:51:10 +0200

    Enable MathJax

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,25 +18,25 @@ sys.setrecursionlimit(1500)
 
 # -- Project information -----------------------------------------------------
 
-project = 'Hypothesis'
-copyright = '2019, Joeri Hermans'
-author = 'Joeri Hermans'
+project = "Hypothesis"
+copyright = "2019, Joeri Hermans"
+author = "Joeri Hermans"
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.3'
+release = "0.0.3"
 
 
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# extensions coming with Sphinx (named "sphinx.ext.*") or your custom
 # ones.
 extensions = [
-    "sphinx.ext.jsmath"
+    "sphinx.ext.mathjax"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -49,9 +49,9 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'press'
+html_theme = "press"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
