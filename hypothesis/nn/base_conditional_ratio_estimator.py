d4daa67d5bb9fb6ee119044a8837944772d0b513
Joeri Hermans <joeri@joerihermans.com>
2019-10-03 22:05:11 +0200

    Add ConditionalRatioEstimatorBCEWithLogitsLoss

--- hypothesis/nn/base_conditional_ratio_estimator.py
+++ hypothesis/nn/base_conditional_ratio_estimator.py
@@ -27,3 +27,25 @@ class BaseConditionalRatioEstimator(torch.nn.Module, ConditionalRatioEstimator):
     def log_ratio(self, xs, ys):
         r""""""
         raise NotImplementedError
+
+
+
+class ConditionalRatioEstimatorLoss(torch.nn.Module):
+
+    def __init__(self, ratio_estimator, criterion=torch.nn.BCE):
+        self.ratio_estimator = ratio_estimator
+        self.criterion = criterion()
+
+    def forward(self, batch_a, batch_b):
+        raise NotImplementedError
+
+
+
+class ConditionalRatioEstimatorBCEWithLogitsLoss(torch.nn.Module):
+
+    def __init__(self, ratio_estimator):
+        self.ratio_estimator = ratio_estimator
+        self.criterion = torch.nn.BCEWithLogitsLoss()
+
+    def forward(self, batch_a, batch_b):
+        raise NotImplementedError
