afca38ff48ec8b6f2256d0ba42e83c88dcb0bf07
Joeri Hermans <joeri@joerihermans.com>
2019-10-03 18:17:49 +0200

    Add AALRMetropolisHastings

--- hypothesis/inference/mcmc.py
+++ hypothesis/inference/mcmc.py
@@ -45,14 +45,65 @@ class MetropolisHastings(MarkovChainMonteCarlo):
         self.log_likelihood = log_likelihood
         self.proposal = proposal
         self.transition = transition
+        self.denominator = None
 
     def _step(self, observations, theta):
         accepted = False
 
-        lnl_theta = self.log_likelihood(theta, observations)
         theta_next = self.transition.sample(theta)
         lnl_theta_next = self.log_likelihood(theta_next, observations)
-        acceptance_ratio = (
-            (self.prior.log_prob(theta_next) + lnl_theta_next)
-            -
-            (self.prior.log_prob(theta) + lnl_theta))
+        numerator = self.prior.log_prob(theta_next) + lnl_theta_next
+        if self.denominator is None:
+            lnl_theta = self.log_likelihood(theta, observations)
+            self.denominator = self.prior.log_prob(theta) + lnl_theta
+        acceptance_ratio = (numerator - self.denominator)
+        if not self.transition.is_symmetrical():
+            raise NotImplementedError
+        acceptance_probability = min([1, acceptance_ratio.exp().item()])
+        u = np.random.uniform()
+        if u <= acceptance_probability:
+            accepted = True
+            theta = theta_next
+
+        return theta, acceptance_probability, accepted
+
+
+
+class AALRMetropolisHastings(MarkovChainMonteCarlo):
+    r"""Ammortized Approximate Likelihood Ratio Metropolis Hastings
+
+    """
+
+    def __init__(self, prior, ratio_estimator, transition):
+        super(RatioEstimatorMetropolisHastings, self).__init__()
+        self.prior = prior
+        self.ratio_estimator = ratio_estimator
+        self.transition = transition
+        self.denominator = None
+
+    def _compute_ratio(self, observations, theta):
+        num_observations = len(observations)
+        thetas = theta.repeat(num_observations, 1)
+        _, log_ratio = self.ratio_estimator(thetas, observations)
+
+        return log_ratio.sum().cpu()
+
+    def _step(self, observations, theta):
+        accepted = False
+
+        theta_next = self.transition.sample(theta)
+        lnl_theta_next = self._compute_ratio(theta_next, observations)
+        numerator = self.prior.log_prob(theta_next) + lnl_theta_next
+        if self.denominator is None:
+            lnl_theta = self._compute_ratio(theta, observations)
+            self.denominator = self.prior.log_prob(theta) + lnl_theta
+        acceptance_ratio = (numerator - self.denominator)
+        if not self.transition.is_symmetrical():
+            raise NotImplementedError
+        acceptance_probability = min([1, acceptance_ratio.exp().item()])
+        u = np.random.uniform()
+        if u <= acceptance_probability:
+            accepted = True
+            theta = theta_next
+
+        return theta, acceptance_probability, accepted
