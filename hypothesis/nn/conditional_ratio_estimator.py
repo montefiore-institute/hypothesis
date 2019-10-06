666725a1e9e118adec6db24cc5c2100bb0825ffe
Joeri Hermans <joeri@joerihermans.com>
2019-10-06 17:44:19 +0200

    Fix ConditionalRatioEstimator criterions

--- hypothesis/nn/conditional_ratio_estimator.py
+++ hypothesis/nn/conditional_ratio_estimator.py
@@ -34,10 +34,11 @@ class BaseConditionalRatioEstimator(torch.nn.Module, ConditionalRatioEstimator):
 class ConditionalRatioEstimatorCriterion(torch.nn.Module):
 
     def __init__(self, ratio_estimator, batch_size):
+        super(ConditionalRatioEstimatorCriterion, self).__init__()
         self.batch_size = batch_size
         self.criterion = torch.nn.BCELoss()
-        self.ones = torch.ones(self.batch_size, 1)
         self.ratio_estimator = ratio_estimator
+        self.ones = torch.ones(self.batch_size, 1)
         self.zeros = torch.zeros(self.batch_size, 1)
 
     def forward(self, batch_a, batch_b):
@@ -47,8 +48,8 @@ class ConditionalRatioEstimatorCriterion(torch.nn.Module):
         y_independent_a, _ = self.ratio_estimator(inputs_a, outputs_b)
         y_dependent_b, _ = self.ratio_estimator(inputs_b, outputs_b)
         y_independent_b, _ = self.ratio_estimator(inputs_b, outputs_a)
-        loss_a = self.criterion(y_dependent_a, ones) + self.criterion(y_independent_a, zeros)
-        loss_b = self.criterion(y_dependent_b, ones) + self.criterion(y_independent_b, zeros)
+        loss_a = self.criterion(y_dependent_a, self.ones) + self.criterion(y_independent_a, self.zeros)
+        loss_b = self.criterion(y_dependent_b, self.ones) + self.criterion(y_independent_b, self.zeros)
         loss = loss_a + loss_b
 
         return loss
@@ -58,11 +59,12 @@ class ConditionalRatioEstimatorCriterion(torch.nn.Module):
 class ConditionalRatioEstimatorLogitsCriterion(torch.nn.Module):
 
     def __init__(self, ratio_estimator, batch_size):
+        super(ConditionalRatioEstimatorLogitsCriterion, self).__init__()
         self.batch_size = batch_size
         self.ratio_estimator = ratio_estimator
         self.criterion = torch.nn.BCEWithLogitsLoss()
-        self.ones = torch.ones(self.batch_size, 1)
         self.ratio_estimator = ratio_estimator
+        self.ones = torch.ones(self.batch_size, 1)
         self.zeros = torch.zeros(self.batch_size, 1)
 
     def forward(self, batch_a, batch_b):
@@ -72,8 +74,8 @@ class ConditionalRatioEstimatorLogitsCriterion(torch.nn.Module):
         _, y_independent_a = self.ratio_estimator(inputs_a, outputs_b)
         _, y_dependent_b = self.ratio_estimator(inputs_b, outputs_b)
         _, y_independent_b = self.ratio_estimator(inputs_b, outputs_a)
-        loss_a = self.criterion(y_dependent_a, ones) + self.criterion(y_independent_a, zeros)
-        loss_b = self.criterion(y_dependent_b, ones) + self.criterion(y_independent_b, zeros)
+        loss_a = self.criterion(y_dependent_a, self.ones) + self.criterion(y_independent_a, self.zeros)
+        loss_b = self.criterion(y_dependent_b, self.ones) + self.criterion(y_independent_b, self.zeros)
         loss = loss_a + loss_b
 
         return loss
