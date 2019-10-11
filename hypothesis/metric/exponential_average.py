b4a06a04558a98ae64156b2ad1ee30fce067a037
Joeri Hermans <joeri@joerihermans.com>
2019-10-11 13:37:58 +0200

    Modify initial value requirementS

--- hypothesis/metric/exponential_average.py
+++ hypothesis/metric/exponential_average.py
@@ -9,11 +9,10 @@ class ExponentialAverage(BaseMetric):
     r""""""
 
     def __init__(self, initial_value=None, alpha=.95):
-        if initial_value is None:
-            raise ValueError("No initial value has been specified.")
         self.initial_value = initial_value
         self.current_value = initial_value
-        self.history = [self.current_value]
+        if initial_value is not None:
+            self.history = [self.current_value]
 
     def update(self, value):
         next_value = alpha * value + (1 - alpha) * self.current_value
@@ -22,7 +21,8 @@ class ExponentialAverage(BaseMetric):
 
     def reset(self):
         self.current_value = self.initial_value
-        self.history = [self.initial_value]
+        if initial_value is not None:
+            self.history = [self.initial_value]
 
     def __getitem__(self, pattern):
         return self.history[pattern]
