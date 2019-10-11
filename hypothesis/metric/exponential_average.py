872f1d31122dcf8531b5cd0c15fed684f9d6d1c6
Joeri Hermans <joeri@joerihermans.com>
2019-10-11 13:42:23 +0200

    Update decay initialization

--- hypothesis/metric/exponential_average.py
+++ hypothesis/metric/exponential_average.py
@@ -8,14 +8,15 @@ from hypothesis.metric import BaseMetric
 class ExponentialAverage(BaseMetric):
     r""""""
 
-    def __init__(self, initial_value=None, alpha=.95):
+    def __init__(self, initial_value=None, decay=.95):
+        self.decay = decay
         self.initial_value = initial_value
         self.current_value = initial_value
         if initial_value is not None:
             self.history = [self.current_value]
 
     def update(self, value):
-        next_value = alpha * value + (1 - alpha) * self.current_value
+        next_value = self.decay * value + (1 - self.decay) * self.current_value
         self.history.append(next_value)
         self.current_value = next_value
 
