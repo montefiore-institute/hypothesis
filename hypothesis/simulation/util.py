a867b16de05f920d118b41425d8e20853df96272
Joeri Hermans <joeri@joerihermans.com>
2019-10-13 18:28:19 +0200

    Add likelihood sampler

--- hypothesis/simulation/util.py
+++ hypothesis/simulation/util.py
@@ -12,7 +12,8 @@ def sample_joint(simulator, prior, n=1):
 
 
 def joint_sampler(simulator, prior):
-    yield sample_joint(simulator, prior)
+    r""""""
+    yield sample_joint(simulator, prior, n=1)
 
 
 
@@ -25,4 +26,20 @@ def sample_marginal(simulator, prior, n=1):
 
 
 def marginal_sampler(simulator, prior):
-    yield sample_marginal(simulator, prior)
+    r""""""
+    yield sample_marginal(simulator, prior, n=1)
+
+
+
+def sample_likelihood(simulator, input, n=1):
+    r""""""
+    inputs = input.view(1, -1).repeat(n, 1)
+    outputs = simulator(inputs)
+
+    return outputs
+
+
+
+def likelihood_sampler(simulator, input):
+    r""""""
+    yield sample_likelihood(simulator, input, n=1)
