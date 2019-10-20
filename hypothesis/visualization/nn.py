36329e0ff5b0776c77a893703c018a77918281eb
Joeri Hermans <joeri@joerihermans.com>
2019-10-21 00:44:58 +0200

    Modify view

---
+++ hypothesis/visualization/nn.py
@@ -0,0 +1,8 @@
+r""""""
+
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+
+from hypothesis.visualization.util import make_square
+from hypothesis.visualization.util import set_aspect
