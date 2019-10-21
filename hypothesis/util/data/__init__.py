22c0537b998d11b2d199919e344eea97734f35e7
Joeri Hermans <joeri@joerihermans.com>
2019-10-21 09:36:00 +0200

    Fix typo

--- hypothesis/util/data/__init__.py
+++ hypothesis/util/data/__init__.py
@@ -2,5 +2,5 @@ r""""""
 
 from hypothesis.util.data.numpy.simulation_dataset import NumpySimulationDataset
 #from hypothesis.util.data.pandas.simulation_dataset import PandasSimulationDataset
+from hypothesis.util.data.simulation_tensor_dataset import SimulationTensorDataset
 from hypothesis.util.data.simulator_dataset import SimulatorDataset
-from hypothesis.util.data.simultion_tensor_dataset import SimulationTensorDataset
