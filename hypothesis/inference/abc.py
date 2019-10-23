c69d3b55e5931ce72837e356e738fb9068bcbb51
Joeri Hermans <joeri@joerihermans.com>
2019-10-23 23:13:10 +0200

    Add ABC

--- hypothesis/inference/abc.py
+++ hypothesis/inference/abc.py
@@ -7,10 +7,37 @@ import torch
 
 class ApproximateBayesianComputation:
 
-    def __init__(self, prior, summary):
+    def __init__(self, simulator, prior, summary, acceptor):
         super(ApproximateBayesianComputation, self).__init__()
+        # Main classical ABC properties.
+        self.acceptor = acceptor
         self.prior = prior
+        self.simulator = simulator
         self.summary = summary
+        # Sampler properties.
+        self.summary_observation = None
+        self.reset()
+
+    def _draw_posterior_sample(self):
+        sample = None
+
+        while sample is None:
+            prior_sample = self.prior.sample()
+            x = self.simulator(prior_sample)
+            s = self.summary(x)
+            if self.acceptor(s, self.summary_observation):
+                sample = prior_sample
+
+        return sample
+
+    def reset(self):
+        self.summary_observation = None
 
     def sample(self, observation, num_samples):
-        raise NotImplementedError
+        samples = []
+
+        self.summary_observation = self.summary(observation)
+        for _ in range(num_samples):
+            samples.append(self._draw_posterior_sample())
+
+        return samples
