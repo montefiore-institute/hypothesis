fed69acf9bdbaa21d7181d8d4544c37fa4c54b5e
Joeri Hermans <joeri@joerihermans.com>
2019-10-24 09:00:46 +0200

    Add AVO stub

---
+++ hypothesis/inference/avo.py
@@ -0,0 +1,23 @@
+import hypothesis
+import torch
+
+from hypothesis.engine import Procedure
+
+
+
+class AdversarialVariationalOptimization(Procedure):
+    r"""Adversarial Variational Optimization
+
+    An implementation of arxiv.org/abs/1707.07113"""
+
+    def __init__(self, simulator,
+        discriminator,
+        gamma=10.0,
+        baseline=None):
+        super(AdversarialVariationalOptimization, self).__init__()
+        self.discriminator = discriminator
+        self.simulator = simulator
+        if not baseline:
+            baseline = AVOBaseline(discriminator)
+        self.baseline = baseline
+        self.gamma = gamma
