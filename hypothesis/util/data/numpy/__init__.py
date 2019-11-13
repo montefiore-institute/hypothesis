300ea8df35c85fab6b8e31cf7f776c1d50edd1ff
Joeri Hermans <joeri@joerihermans.com>
2019-11-13 11:23:15 +0100

    Update numpy data init

--- hypothesis/util/data/numpy/__init__.py
+++ hypothesis/util/data/numpy/__init__.py
@@ -1,5 +1,5 @@
-from .simulation_dataset import SimulationDataset
 from .storage import Storage
+from .simulation_dataset import SimulationDataset
 from .util import compute_final_shape
 from .util import merge
 from .util import write
