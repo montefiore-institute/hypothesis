7cdb59b2e9f64799ecdfc9a7d038170b9378ab4b
Joeri Hermans <joeri@joerihermans.com>
2019-11-13 11:13:16 +0100

    Fix util init

--- hypothesis/util/data/__init__.py
+++ hypothesis/util/data/__init__.py
@@ -4,6 +4,7 @@ r""""""
 
 #from hypothesis.util.data.pandas.simulation_dataset import PandasSimulationDataset
 from hypothesis.util.data.distribution_dataset import DistributionDataset
-from hypothesis.util.data.numpy.simulation_dataset import NumpySimulationDataset
+from hypothesis.util.data.numpy.simulation_dataset import SimulationDataset as NumpySimulationDataset
+from hypothesis.util.data.numpy.dataset import Dataset as NumpyDataset
 from hypothesis.util.data.simulation_tensor_dataset import SimulationTensorDataset
 from hypothesis.util.data.simulator_dataset import SimulatorDataset
