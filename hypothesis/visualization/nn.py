6f8388b7be4eb1498560cb3c3d463e3c39f043f2
Joeri Hermans <joeri@joerihermans.com>
2019-12-11 12:53:51 +0100

    Update loss plots

--- hypothesis/visualization/nn.py
+++ hypothesis/visualization/nn.py
@@ -24,13 +24,13 @@ def plot_losses(losses_train, losses_test, epochs=None, log=True, figsize=None):
                 num_epochs = losses_train.shape[1]
             else:
                 num_epochs = losses_train.shape[0]
-            epochs = np.arange(1, num_epochs)
+            epochs = np.arange(1, num_epochs + 1)
         # Allocate a figure with shared y-axes.
         figure, axes = plt.subplots(nrows=1, ncols=2, figsize=figsize, sharey=True)
         plot_loss(axes[0], losses_train, epochs=epochs, title="Training loss",
-            xlabel="Epochs", ylabel=ylabel, log=log)
+            xlabel="Epochs", ylabel=ylabel)
         plot_loss(axes[1], losses_test, epochs=epochs, title="Test loss",
-            xlabel="Epochs", ylabel=ylabel, log=log)
+            xlabel="Epochs", ylabel=None)
         figure.tight_layout()
 
     return figure
@@ -39,6 +39,15 @@ def plot_losses(losses_train, losses_test, epochs=None, log=True, figsize=None):
 def plot_loss(ax, losses, epochs=None, title=None, xlabel=None, ylabel=None):
     with torch.no_grad():
         # Check if the standard deviation needs to be computed.
-        mean = losses.mean(dim=)
-        ax.legend()
+        if losses.dim() == 2 and losses.shape[1] > 1:
+            mean, std = losses.mean(dim=0), losses.std(dim=0)
+            ax.plot(epochs, mean, color="black", lw=2, label="Loss")
+            ax.fill_between(epochs, mean - std, mean + std, alpha=.25, color="black", label=r"$\pm1\sigma$ loss")
+            ax.legend()
+        else:
+            ax.plot(epochs, losses.numpy(), color="black", lw=2)
+        ax.set_title(title)
+        ax.set_xlabel(xlabel)
+        ax.set_ylabel(ylabel)
+        ax.minorticks_on()
         make_square(ax)
