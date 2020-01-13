8afeb46aebd12d620e877c917b38e34c36a3f16d
Joeri Hermans <joeri@joerihermans.com>
2020-01-13 11:38:28 +0100

    Check for the presence of the file descriptor in persistent storages

--- hypothesis/util/data/numpy/storage.py
+++ hypothesis/util/data/numpy/storage.py
@@ -68,7 +68,7 @@ class PersistentStorage(BaseStorage):
         return data.reshape(self.data_shape)
 
     def close(self):
-        if self.fd is not None:
+        if hasattr(self, "fd") self.fd is not None:
             self.fd.close()
         self.fd = None
 
