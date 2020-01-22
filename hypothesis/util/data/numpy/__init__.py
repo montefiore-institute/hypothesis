2f3ba3e32ca2c84b0189ef9eb32a46f3d7927f00
Joeri Hermans <joeri@joerihermans.com>
2020-01-22 12:38:22 +0100

    Update __init__

--- hypothesis/util/data/numpy/__init__.py
+++ hypothesis/util/data/numpy/__init__.py
@@ -3,5 +3,4 @@ from .storage import PersistentStorage
 from .simulation_dataset import SimulationDataset
 from .util import compute_final_shape
 from .util import merge
-from .util import write
 from .dataset import Dataset
