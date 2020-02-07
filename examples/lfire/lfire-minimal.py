8d5001afdd50d6de47eb6030a46b0f6f2e2583b2
Joeri Hermans <joeri@joerihermans.com>
2020-02-07 08:57:33 +0100

    Update tractable benchmark

---
+++ examples/lfire/lfire-minimal.py
@@ -0,0 +1,9 @@
+r"""A minimal example of LFIRE (Likelihood-Free Inference By Ratio Estimation).
+
+"""
+
+import torch
+import numpy
+import hypothesis
+
+from hypothesis.inference.lfire import LFIRE
