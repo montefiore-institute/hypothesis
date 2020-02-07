938823acc4839b8aa8b554b31f260552e99d55cc
Joeri Hermans <joeri@joerihermans.com>
2020-02-07 09:22:26 +0100

    Update LFIRE example

--- examples/lfire/lfire-minimal.py
+++ examples/lfire/lfire-minimal.py
@@ -2,8 +2,28 @@ r"""A minimal example of LFIRE (Likelihood-Free Inference By Ratio Estimation).
 
 """
 
-import torch
-import numpy
 import hypothesis
+import numpy
+import torch
 
+from hypothesis.benchmark.normal import Simulator
+from hypothesis.benchmark.normal import allocate_prior
+from hypothesis.benchmark.normal import allocate_truth
 from hypothesis.inference.lfire import LFIRE
+
+
+
+def main(arguments):
+    raise NotImplementedError
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser("LFIRE Posterior Inference: minimal example on a tractable problem.")
+    arguments, _ = parser.parse_known_args()
+
+    return arguments
+
+
+if __name__ == "__main__":
+    arguments = parse_arguments()
+    main(arguments)
