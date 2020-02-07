767e5774e4c642137e97dd08800444f4b2f7b74d
Joeri Hermans <joeri@joerihermans.com>
2020-02-07 15:03:45 +0100

    Add default allocation of neuromodulation controller

--- hypothesis/nn/neuromodulation/__init__.py
+++ hypothesis/nn/neuromodulation/__init__.py
@@ -1,4 +1,5 @@
 from .base import BaseNeuromodulatedModule
+from .controller import default_neuromodulation_controller
 from .elu import NeuromodulatedELU
 from .relu import NeuromodulatedReLU
 from .selu import NeuromodulatedSELU
