4fed3ebbcb02856d37a29e07035cc10ccac7a347
Joeri Hermans <joeri@joerihermans.com>
2020-02-08 18:50:49 +0100

    Add mutual information criterion

---
+++ hypothesis/nn/amortized_ratio_estimation/mutual_information.py
@@ -0,0 +1,37 @@
+import hypothesis
+import hypothesis.nn
+import torch
+
+from .base import BaseCriterion
+from .base import BaseAmortizedRatioEstimator
+
+
+
+class MutualInformationCriterion(BaseCriterion):
+
+    DENOMINATOR = "x|y"
+
+    def __init__(self,
+        estimator,
+        batch_size=hypothesis.default.batch_size,
+        logits=False):
+        super(LikelihoodToEvidenceCriterion, self).__init__(
+            batch_size=batch_size,
+            denominator=LikelihoodToEvidenceCriterion.DENOMINATOR,
+            estimator=estimator,
+            logits=logits)
+
+
+
+class LikelihoodToEvidenceAmortizedRatioEstimator(BaseAmortizedRatioEstimator):
+
+    def __init__(self):
+        super(LikelihoodToEvidenceAmortizedRatioEstimator, self).__init__()
+
+    def forward(self, x, y):
+        log_ratios = self.log_ratio(x=x, y=y)
+
+        return log_ratios.sigmoid(), log_ratios
+
+    def log_ratio(self, x, y):
+        raise NotImplementedError
