04668f9924e4b79d19561e7373fe440c544d9dcb
Joeri Hermans <joeri@joerihermans.com>
2020-02-09 11:10:00 +0100

    Add mutual information MLP ratio estimator

---
+++ hypothesis/nn/amortized_ratio_estimation/multi_layered_perceptron/mutual_information.py
@@ -0,0 +1,32 @@
+import hypothesis
+import hypothesis.nn
+import torch
+
+from hypothesis.nn import MultiLayeredPerceptron
+from hypothesis.nn.amortized_ratio_estimation import BaseMutualInformationRatioEstimator
+from hypothesis.nn.util import compute_dimensionality
+
+
+
+class MutualInformationAmortizedRatioEstimatorMLP(BaseMutualInformationAmortizedRatioEstimator):
+
+    def __init__(self,
+        shape_inputs,
+        shape_outputs,
+        activation=hypothesis.default.activation,
+        dropout=hypothesis.default.dropout,
+        layers=hypothesis.default.trunk):
+        super(MutualInformationRatioEstimatorMLP, self).__init__()
+        dimensionality = compute_dimensionality(shape_inputs) + compute_dimensionality(shape_outputs)
+        self.mlp = MultiLayeredPerceptron(
+            shape_xs=(dimensionality,),
+            shape_ys=(1,),
+            activation=activation,
+            dropout=dropout,
+            layers=layers,
+            transform_output=None)
+
+    def log_ratio(self, x, y):
+        features = torch.cat([x, y], dim=1)
+
+        return self.mlp(features)
