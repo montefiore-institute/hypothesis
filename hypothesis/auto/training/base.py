1994d452590adffca25aadbe0c0ba50ec9816a0d
Joeri Hermans <joeri@joerihermans.com>
2020-02-10 13:41:09 +0100

    Update BaseTrainer

--- hypothesis/auto/training/base.py
+++ hypothesis/auto/training/base.py
@@ -1,6 +1,4 @@
 import hypothesis
-import os
-import torch
 
 from hypothesis.engine import Procedure
 
@@ -9,38 +7,22 @@ from hypothesis.engine import Procedure
 class BaseTrainer(Procedure):
 
     def __init__(self,
-        criterion,
-        dataset_train,
-        feeder,
-        optimizer,
         batch_size=hypothesis.default.batch_size,
         checkpoint=None,
-        dataset_test=None,
         epochs=hypothesis.default.epochs,
-        lr_scheduler=None,
         workers=hypothesis.default.dataloader_workers):
         super(BaseTrainer, self).__init__()
-        # Datasets
-        self.dataset_train = dataset_train
-        self.dataset_test = dataset_test
         # Training hyperparameters
         self.batch_size = batch_size
-        self.epochs = epochs
         self.checkpoint_path = checkpoint
         self.dataloader_workers = workers
+        self.epochs = epochs
         # Trainer state
-        self.criterion = criterion
         self.current_epoch = 0
         self.epochs_remaining = self.epochs
-        self.estimator = criterion.estimator
-        self.feeder = feeder
-        self.losses_test = []
-        self.losses_train = []
-        self.lr_scheduler = lr_scheduler
-        self.optimizer = optimizer
-        self.best_epoch = None
-        self.best_loss = float("infinity")
-        self.best_model = None
+        # Load the previously saved state.
+        if self.checkpoint_path is not None:
+            self._checkpoint_load()
 
     def _checkpoint_store(self):
         raise NotImplementedError
@@ -65,40 +47,10 @@ class BaseTrainer(Procedure):
         raise NotImplementedError
 
     def optimize(self):
-        for epoch in range(self.epochs_remaining):
-            self.current_epoch = epoch
-            self.train()
-            # Check if a testing dataset is available.
-            if self.dataset_test is not None:
-                self.test()
-            if self.lr_scheduler is not None:
-                self.lr_scheduler.step()
-            # self.checkpoint()
-
-        return self._summarize()
+        raise NotImplementedError
 
     def test(self):
-        self.estimator.test()
-        loader = self._allocate_data_loader(self.dataset_test)
-        total_loss = 0.0
-        for batch in loader:
-            loss = self.feeder(batch)
-            total_loss += loss.item()
-        total_loss /= len(dataset_test)
-        if total_loss < self.loss_best:
-            state_dict = self.estimator.state_dict()
-            for k, v in state_dict.items():
-                state_dict[k] = v.cpu()
-            self.best_loss = total_loss
-            self.best_model = state_dict
-            self.best_epoch = self.current_epoch
+        raise NotImplementedError
 
     def train(self):
-        self.estimator.train()
-        loader = self._allocate_data_loader(self.dataset_train)
-        for batch in loader:
-            loss = self.feeder(batch)
-            self.optimizer.zero_grad()
-            loss.backward()
-            self.optimizer.step()
-            self.losses_train.append(loss.item())
+        raise NotImplementedError
