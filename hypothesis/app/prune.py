6180977adf9555b0622586bff7dc05e114151e3b
Joeri Hermans <joeri@joerihermans.com>
2020-02-11 08:39:21 +0100

    Update pruning

--- hypothesis/app/prune.py
+++ hypothesis/app/prune.py
@@ -5,29 +5,40 @@ r"""A utility program to prune data files.
 import argparse
 import numpy as np
 import os
+import shutil
 import torch
 
 
 
 def main(arguments):
-    raise NotImplementedError
+    # Check if the file needs to be processed in memory.
+    if arguments.in_memory:
+        mmap_mode = 'r'
+    else:
+        mmap_mode = None
+    data = np.load(arguments.in, mmap_mode=mmap_mode)
+    data = np.delete(data, arguments.indices, arguments.dimension)
+    np.save(arguments.out_file, data)
 
 
 def parse_arguments():
     parser = argparse.ArgumentParser("Prune: pruning data files for you convenience.")
     parser.add_argument("--dimension", type=int, default=1, help="Data dimension to work in (default: 1).")
-    parser.add_argument("--in", type=str, default=None, help="Path to the file to process (default: none).")
+    parser.add_argument("--in-file", type=str, default=None, help="Path to the file to process (default: none).")
     parser.add_argument("--in-memory", action="store_true", help="Process the data in-memory (default: false).")
     parser.add_argument("--indices", type=str, default=None, help="A comma-seperated list of indices to remove (default: none).")
-    parser.add_argument("--out", type=str, default=None, help="Path of the processed file (default: none).")
-    parser.add_argument("--tempfile", type=str, default=None, help="Path of the temporary file to store the intermediate results (default: none).")
+    parser.add_argument("--out-file", type=str, default=None, help="Path of the processed file (default: none).")
     arguments, _ = parser.parse_known_args()
     # Check if an input file has been specified.
-    if arguments.in is None:
+    if arguments.in_file is None:
         raise ValueError("No input file has been specified.")
     # Check if an output file has been specified.
-    if arguments.out is None:
+    if arguments.out_file is None:
         raise ValueError("No output file has been specified.")
+    # Check if indices have been specified.
+    if arguments.indices is None:
+        raise ValueError("No indices have been specified.")
+    arguments.indices = [int(index) for index in arguments.indices.split(',')]
 
     return arguments
 
