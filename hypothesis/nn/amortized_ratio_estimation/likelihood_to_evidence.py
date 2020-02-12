f272047ec21064275ab7a2d511500d9ff15b6dfa
Joeri Hermans <joeri@joerihermans.com>
2020-02-12 11:26:12 +0100

    Fix criterion

--- hypothesis/nn/amortized_ratio_estimation/likelihood_to_evidence.py
+++ hypothesis/nn/amortized_ratio_estimation/likelihood_to_evidence.py
@@ -39,14 +39,14 @@ class LikelihoodToEvidenceSwappingCriterion(BaseCriterion):
 
     def _forward_without_logits(self, inputs, outputs):
         # A pass
-        y_dependent, _ = self.ratio_estimator(inputs, outputs) # Dependent samples
+        y_dependent, _ = self.estimator(inputs, outputs) # Dependent samples
         outputs_random = outputs[torch.randperm(self.batch_size)].detach()
-        y_independent, _ = self.ratio_estimator(inputs, outputs_random) # Independent samples
+        y_independent, _ = self.estimator(inputs, outputs_random) # Independent samples
         loss_a = self.criterion(y_dependent, self.ones) + self.criterion(y_independent, self.zeros)
         # B pass
-        y_dependent, _ = self.ratio_estimator(inputs, outputs) # Dependent samples
+        y_dependent, _ = self.estimator(inputs, outputs) # Dependent samples
         inputs_random = inputs[torch.randperm(self.batch_size)].detach()
-        y_independent, _ = self.ratio_estimator(inputs_random, outputs) # Independent samples
+        y_independent, _ = self.estimator(inputs_random, outputs) # Independent samples
         loss_b = self.criterion(y_dependent, self.ones) + self.criterion(y_independent, self.zeros)
         # Combined
         loss = (loss_a + loss_b) / 2
@@ -55,14 +55,14 @@ class LikelihoodToEvidenceSwappingCriterion(BaseCriterion):
 
     def _forward_with_logits(self, inputs, outputs):
         # A pass
-        _, y_dependent = self.ratio_estimator(inputs, outputs) # Dependent samples
+        _, y_dependent = self.estimator(inputs, outputs) # Dependent samples
         outputs_random = outputs[torch.randperm(self.batch_size)].detach()
-        _, y_independent = self.ratio_estimator(inputs, outputs_random) # Independent samples
+        _, y_independent = self.estimator(inputs, outputs_random) # Independent samples
         loss_a = self.criterion(y_dependent, self.ones) + self.criterion(y_independent, self.zeros)
         # B pass
-        _, y_dependent = self.ratio_estimator(inputs, outputs) # Dependent samples
+        _, y_dependent = self.estimator(inputs, outputs) # Dependent samples
         inputs_random = inputs[torch.randperm(self.batch_size)].detach()
-        _, y_independent = self.ratio_estimator(inputs_random, outputs) # Independent samples
+        _, y_independent = self.estimator(inputs_random, outputs) # Independent samples
         loss_b = self.criterion(y_dependent, self.ones) + self.criterion(y_independent, self.zeros)
         # Combined
         loss = (loss_a + loss_b) / 2
