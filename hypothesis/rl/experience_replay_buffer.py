c9fa0ccc81bd0c7d14357cb630abfdefd72e73ef
Joeri Hermans <joeri@joerihermans.com>
2020-03-24 10:24:00 +0100

    Add sample utility method to experience replay buffer

--- hypothesis/rl/experience_replay_buffer.py
+++ hypothesis/rl/experience_replay_buffer.py
@@ -1,6 +1,5 @@
 import hypothesis
 import numpy as np
-import random
 import torch
 
 from torch.utils.data import Dataset
@@ -18,7 +17,7 @@ class ExperienceReplayBuffer(Dataset):
         self.storages = [[] for _ in range(self.num_storages)]
 
     @torch.no_grad()
-    def _retriever(self, index):
+    def _retrieve(self, index):
         elements = []
         for storage_index in range(self.num_storages):
             elements.append(self.storages[storage_index][index].unsqueeze(dim=0))
@@ -27,14 +26,27 @@ class ExperienceReplayBuffer(Dataset):
 
     @torch.no_grad()
     def put(self, transition):
-        for index in range(len(transition)):
+        for index in range(self.num_storages):
             self.storages[index].append(transition[index].squeeze())
 
     def size(self):
         return len(self.storages[0])
 
+    @torch.no_grad()
+    def sample(self, batch_size=hypothesis.default.batch_size):
+        indices = np.random.randint(0, self.size())
+        tensors = [[] for _ in range(self.num_storages)]
+        experiences = [self._retrieve(index) for index in indices]
+        for experience in experiences:
+            for storage_index, element in enumerate(experience):
+                tensors[storage_index].append(element)
+        for index in range(self.num_storages):
+            tensors[index] = torch.cat(tensors[index], dim=0)
+
+        return tuple(tensors)
+
     def __getitem__(self, index):
-        return self._retriever(index)
+        return self._retrieve(index)
 
     def __len__(self):
         return self.size()
