554ed46aa2c3a31269c8371e0577570ed73001e1
Joeri Hermans <joeri@peinser.com>
2020-06-13 08:41:53 +0200

    Refactor highest density level

--- hypothesis/stat/constraint.py
+++ hypothesis/stat/constraint.py
@@ -4,38 +4,23 @@ import torch
 
 
 @torch.no_grad()
-def highest_density_level(pdf, alpha, epsilon=10e-10):
-    pdf /= pdf.sum()
+def highest_density_level(pdf, alpha, epsilon=10e-10, mask=False):
+    # Prepare posterior
+    total_pdf = pdf.sum().item()
+    pdf /= total_pdf
     pdf = pdf.numpy()
-    if pdf.ndim >= 2:
-        mask = multivariate_density_level(pdf=pdf, alpha=alpha, epsilon=epsilon)
+    # Compute highest density level and the corresponding mask
+    n = len(pdf)
+    area = float(1)
+    optimal_level = float(0)
+    while area > alpha:
+        mask = (pdf >= optimal_level).astype(np.float32)
+        area = np.sum(mask * pdf)
+        optimal_level += epsilon
+    # Restore to original level
+    optimal_level *= total_pdf
+
+    if mask
+        return optimal_level, torch.from_numpy(mask)
     else:
-        mask = univariate_density_level(pdf=pdf, alpha=alpha, epsilon=epsilon)
-
-    return torch.from_numpy(mask)
-
-
-@torch.no_grad()
-def univariate_density_level(pdf, alpha, epsilon=10e-10):
-    n = len(pdf)
-    area = float(1)
-    optimal_level = float(0)
-    while area > alpha:
-        mask = (pdf >= optimal_level).astype(np.float32)
-        area = np.sum(mask * pdf)
-        optimal_level += epsilon
-
-    return mask
-
-
-@torch.no_grad()
-def multivariate_density_level(pdf, alpha, epsilon=10e-10):
-    n = len(pdf)
-    area = float(1)
-    optimal_level = float(0)
-    while area > alpha:
-        mask = (pdf >= optimal_level).astype(np.float32)
-        area = np.sum(mask * pdf)
-        optimal_level += epsilon
-
-    return mask
+        return optimal_level
