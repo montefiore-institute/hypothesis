71d4ed399771c04ab770c641717dde39909297d0
Joeri Hermans <joeri@peinser.com>
2020-06-13 08:18:51 +0200

    Disable gradients when computing credible intervals

--- hypothesis/stat/constraint.py
+++ hypothesis/stat/constraint.py
@@ -4,15 +4,19 @@ import numpy as np
 
 @torch.no_grad()
 def highest_density_level(pdf, alpha, epsilon=10e-10):
-    if pdf.ndim > 2:
-        return multivariate_density_level(pdf=pdf, alpha=alpha, epsilon=epsilon)
+    assert(pdf.sum() == 1)
+
+    pdf = pdf.numpy()
+    if pdf.ndim >= 2:
+        mask = multivariate_density_level(pdf=pdf, alpha=alpha, epsilon=epsilon)
     else:
-        return univariate_density_level(pdf=pdf, alpha=alpha, epsilon=epsilon)
+        mask = univariate_density_level(pdf=pdf, alpha=alpha, epsilon=epsilon)
+
+    return torch.from_numpy(mask)
 
 
 @torch.no_grad()
 def univariate_density_level(pdf, alpha, epsilon=10e-10):
-    assert(pdf.sum() == 0)
     n = len(pdf)
     area = float(1)
     optimal_level = float(0)
@@ -26,4 +30,12 @@ def univariate_density_level(pdf, alpha, epsilon=10e-10):
 
 @torch.no_grad()
 def multivariate_density_level(pdf, alpha, epsilon=10e-10):
-    raise NotImplementedError
+    n = len(pdf)
+    area = float(1)
+    optimal_level = float(0)
+    while area > alpha:
+        mask = (pdf >= optimal_level).astype(np.float32)
+        area = np.sum(mask * pdf)
+        optimal_level += epsilon
+
+    return mask
