f5bf155e9d695bc49ce874d721f7b951e4b57c44
Joeri Hermans <joeri@peinser.com>
2020-06-14 23:59:00 +0200

    Add more freedom to the DensityDiagnostic

--- hypothesis/diagnostic/density.py
+++ hypothesis/diagnostic/density.py
@@ -9,15 +9,21 @@ from scipy.integrate import nquad
 
 class DensityDiagnostic(BaseDiagnostic):
 
-    def __init__(self, space, density, epsilon=0.1):
+    def __init__(self, space, function=None, epsilon=0.1):
         super(DensityDiagnostic, self).__init__()
-        self.density = density
         self.epsilon = epsilon
+        self.function = function
         self.last_result = None
         self.space = space
 
-    def test(**kwargs):
-        self.last_result = integrate.nquad(self.pdf, self.space)
+    def test(function=None):
+        if function is None and self.function is None:
+            raise ValueError("A density function needs to be provided to the method, or through the constructor.")
+        elif function is None and self.function is not None:
+            f = self.function
+        else:
+            f = function
+        self.last_result = integrate.nquad(f, self.space)
         area, _ = self.last_result
 
         return abs(1 - area) <= self.epsilon
