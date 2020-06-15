4c661519ffbf5b154c6dae0a68e7a10f4e91abcc
Joeri Hermans <joeri@peinser.com>
2020-06-15 16:52:09 +0200

    Update DensityDiagnostic

--- hypothesis/diagnostic/density.py
+++ hypothesis/diagnostic/density.py
@@ -12,15 +12,18 @@ class DensityDiagnostic(BaseDiagnostic):
     def __init__(self, space, epsilon=0.1):
         super(DensityDiagnostic, self).__init__()
         self.epsilon = epsilon
+        self.areas = []
         self.results = []
         self.space = space
 
     def reset(self):
+        self.areas = []
         self.results = []
 
     def test(self, function):
-        area, error = integrate.nquad(function, self.space)
+        area, _ = integrate.nquad(function, self.space)
         passed = abs(1 - area) <= self.epsilon
-        self.results.append(result)
+        self.areas.append(area)
+        self.results.append(passed)
 
         return passed
