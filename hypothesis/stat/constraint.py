54ac52e547f8516bb122a52011a5f309a3a099eb
Joeri Hermans <joeri@joerihermans.com>
2020-07-04 00:58:13 +0200

    Update constraints

--- hypothesis/stat/constraint.py
+++ hypothesis/stat/constraint.py
@@ -1,32 +1,30 @@
+import math
 import numpy as np
 import torch
 
 
+
 @torch.no_grad()
-def highest_density_level(pdf, alpha, min_error=10e-5, mask=False, lr=0.0001, mu=0.1):
+def highest_density_level(pdf, alpha, mask=False):
     # Prepare posterior
     pdf = pdf.cpu().clone().numpy() # Clone to fix strange behaviour in Jupyter.
     total_pdf = pdf.sum()
     pdf /= total_pdf
     # Compute highest density level and the corresponding mask
     n = len(pdf)
-    area = float(1)
     optimal_level = float(0)
-    error = float("infinity")
-    momentum = 0.0
-    last_error = error
-    while abs(error) >= min_error:
-        m = (pdf >= optimal_level).astype(np.float32)
-        area = np.sum(m * pdf)
-        error = alpha - area
-        grad = lr * error
-        momentum = grad + mu * momentum
-        optimal_level = optimal_level - momentum
-        if last_error == error:
-            break
-        last_error = error
+    epsilon = 10e-02
+    while epsilon >= 10e-20:
+        area = float(1)
+        while area >= alpha:
+            # Compute the integral
+            m = (pdf >= optimal_level).astype(np.float32)
+            area = np.sum(m * pdf)
+            # Compute the error and apply gradient descent
+            optimal_level += epsilon
+        optimal_level -= 2 * epsilon
+        epsilon /= 10
     optimal_level *= total_pdf
-
     if mask:
         return optimal_level, torch.from_numpy(m)
     else:
