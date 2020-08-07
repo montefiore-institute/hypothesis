efa2276beec96ee4d082d5035cac3a6b1688eb8a
Joeri Hermans <joeri@joerihermans.com>
2020-08-07 03:36:58 +0200

    Build constraints from top instead of bottom

--- hypothesis/stat/constraint.py
+++ hypothesis/stat/constraint.py
@@ -16,17 +16,17 @@ def highest_density_region(pdf, alpha, min_epsilon=10e-17):
     pdf /= total_pdf
     # Compute highest density level and the corresponding mask
     n = len(pdf)
-    optimal_level = float(0)
+    optimal_level = pdf.max()
     epsilon = 10e-02
     while epsilon >= min_epsilon:
         area = float(1)
-        while area >= alpha:
+        while area <= alpha:
             # Compute the integral
             m = (pdf >= optimal_level).astype(np.float32)
             area = np.sum(m * pdf)
             # Compute the error and apply gradient descent
-            optimal_level += epsilon
-        optimal_level -= 2 * epsilon
+            optimal_level -= epsilon
+        optimal_level += 2 * epsilon
         epsilon /= 10
 
     return torch.from_numpy(m)
@@ -44,17 +44,17 @@ def highest_density_level(pdf, alpha, min_epsilon=10e-17, region=False):
     pdf /= total_pdf
     # Compute highest density level and the corresponding mask
     n = len(pdf)
-    optimal_level = float(0)
+    optimal_level = pdf.max()
     epsilon = 10e-02
     while epsilon >= min_epsilon:
         area = float(1)
-        while area >= alpha:
+        while area <= alpha:
             # Compute the integral
             m = (pdf >= optimal_level).astype(np.float32)
             area = np.sum(m * pdf)
             # Compute the error and apply gradient descent
-            optimal_level += epsilon
-        optimal_level -= 2 * epsilon
+            optimal_level -= epsilon
+        optimal_level += 2 * epsilon
         epsilon /= 10
     optimal_level *= total_pdf
     if region:
