2ece831a3c750fe60d0e29aa749f8101a646043e
Joeri Hermans <joeri@joerihermans.com>
2020-10-05 11:35:08 +0200

    Update utility

--- hypothesis/util/data/numpy/util.py
+++ hypothesis/util/data/numpy/util.py
@@ -32,7 +32,7 @@ def merge_in_memory(input_files, output_file, shape, dtype=None, axis=0):
     np.save(output_file, datamap)
 
 
-def merge_on_disk(input_files, output_files, shape, dtype=None, axis=0, tempfile=None):
+def merge_on_disk(input_files, output_file, shape, dtype=None, axis=0, tempfile=None):
     # Check if a random temporary file needs to be allocated.
     if tempfile is None:
         _, tempfile = temp.mkstemp(dir='.')
