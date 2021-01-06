4cb7f7a61a27ba83f509250388feb348789633cf
Joeri Hermans <joeri@joerihermans.com>
2021-01-06 13:10:22 +0100

    Update gitignore

--- setup.py
+++ setup.py
@@ -7,9 +7,6 @@ from setuptools import setup, find_packages
 with open('README.rst') as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
-    history = history_file.read()
-
 requirements = [ ]
 
 setup_requirements = ['pytest-runner', ]
@@ -39,7 +36,7 @@ setup(
     },
     install_requires=requirements,
     license="BSD license",
-    long_description=readme + '\n\n' + history,
+    long_description=readme,
     include_package_data=True,
     keywords='hypothesis',
     name='hypothesis',
