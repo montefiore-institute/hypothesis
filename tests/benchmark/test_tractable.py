2600e0c32faa11883262b709fece3dcad60087d4
Joeri Hermans <joeri@joerihermans.com>
2021-01-07 13:46:17 +0100

    Update

--- tests/benchmark/test_tractable.py
+++ tests/benchmark/test_tractable.py
@@ -16,3 +16,16 @@ from hypothesis.benchmark.tractable import Simulator
 def test_simulator():
     prior = Prior()
     simulator = Simulator()
+    # Test a single sample
+    inputs = prior.sample()
+    simulator(inputs)
+    outputs = simulator.forward(inputs)
+    assert outputs.shape[0] == 1
+    assert outputs.shape[1] == 8
+    # Test multiple samples
+    n = 100
+    inputs = prior.sample((n,))
+    simulator(inputs)
+    outputs = simulator.forward(inputs)
+    assert outputs.shape[0] == n
+    assert outputs.shape[1] == 8
