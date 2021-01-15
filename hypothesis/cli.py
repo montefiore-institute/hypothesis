c970dac97ac92a52ede8a3c9afa727349218e38f
Joeri Hermans <joeri@joerihermans.com>
2021-01-15 11:40:13 +0100

    Update cli

--- hypothesis/cli.py
+++ hypothesis/cli.py
@@ -6,18 +6,23 @@ import sys
 
 
 def main():
-    modules = ["merge", "prune", "version"]
+    modules = ["config", "merge", "prune", "version"]
     # Check if a command line option has been specified.
     if len(sys.argv) == 1 or not any(m in sys.argv for m in modules):
         show_help_and_exit()
 
     # Define the mapping between the command and executable functions.
     mapping = {
+        "config": execute_config,
         "merge": execute_merge,
         "prune": execute_prune,
         "version": execute_version}
     # Execute the command, if it exists.
     mapping[sys.argv[1]]()
+
+
+def execute_config():
+    raise Exception("TODO")
 
 
 def execute_merge():
@@ -38,6 +43,7 @@ def show_help_and_exit():
     help = r"""hypothesis """ + h.__version__ + """
 
 Modules:
+  config     Configuration module, adjust your hypothesis configuration.
   merge      Utility program to merge data files.
   prune      Utility program to prune data files.
   version    Displays the current version of the hypothesis CLI.
