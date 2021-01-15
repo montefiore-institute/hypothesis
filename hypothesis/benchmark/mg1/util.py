cc88ce0dd4b7d7dddb3536673a087c8d2ef26124
Joeri Hermans <joeri@joerihermans.com>
2021-01-15 10:42:28 +0100

    Add M/G/1 benchmark

---
+++ hypothesis/benchmark/mg1/util.py
@@ -0,0 +1,32 @@
+r"""Utilities for the M/G/1 benchmark.
+
+"""
+
+import torch
+
+
+@torch.no_grad()
+def Prior():
+    r"""Returns a uniform prior between `(0, 0, 0)` and
+    `(10, 10, 1/3)`. """
+    lower = torch.tensor([0.0, 0.0, 0.0])
+    upper = torch.tensor([10.0, 10.0, 1 / 3])
+
+    return Uniform(lower, upper)
+
+
+@torch.no_grad()
+def Truth():
+    r"""Returns the true queuing model parameters: ``(1, 5, 0.2)``.
+
+    """
+    return torch.tensor([1.0, 5.0, 0.2])
+
+
+class Uniform(torch.distributions.uniform.Uniform):
+
+    def __init__(self, lower, upper):
+        super(Uniform, self).__init__(lower, upper)
+
+    def log_prob(self, sample):
+        return super(Uniform, self).log_prob(sample).sum()
