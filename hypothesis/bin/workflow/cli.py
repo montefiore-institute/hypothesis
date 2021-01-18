9d487defd1b4310145a67c26da19d7943167b10f
Joeri Hermans <joeri@joerihermans.com>
2021-01-19 00:54:41 +0100

    Update workflow CLI tool

--- hypothesis/bin/workflow/cli.py
+++ hypothesis/bin/workflow/cli.py
@@ -7,6 +7,7 @@ import hypothesis as h
 import hypothesis.workflow as w
 import hypothesis.workflow.local
 import hypothesis.workflow.slurm
+import logging
 import os
 import sys
 
@@ -14,28 +15,57 @@ import sys
 def main():
     executor = load_default_executor()
     arguments = parse_arguments()
-    if arguments.local:
-        executor = w.local.execute
     if arguments.slurm:
-        executor = w.slurm.execute
+        executor = "slurm"
+    if arguments.local:
+        executor = "local"
+    executors = {
+        "slurm": execute_slurm,
+        "local": execute_local}
+    if len(arguments.args) > 0:
+        script = arguments.args[0]
+        exec(open(script).read())
+        executors[executor](arguments)
+
+
+def execute_slurm(arguments):
+    logging.info("Using the Slurm workflow backend.")
+
+
+def execute_local(arguments):
+    logging.info("Using the local workflow backend.")
 
 
 def load_default_executor():
     if hypothesis.workflow.slurm.slurm_detected():
-        executor = w.slurm.execute
+        return "slurm"
     else:
-        executor = w.local.execute
-
-    return executor
+        return "local"
 
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
     # Slurm backend configuration
+    parser.add_argument("--directory", type=str, default=None, help="Directory to generate the Slurm submission scripts.")
+    # Logging options
+    parser.add_argument("--level", default="warning", type=str, help="Minimum logging level (default: warning) (options: debug, info, warning, error, critical).")
     # Executor backend
     parser.add_argument("--slurm", action="store_true", help="Force the usage the Slurm executor backend.")
     parser.add_argument("--local", action="store_true", help="Force the usage the local executor backend.")
-    arguments, _ = parser.parse_known_args()
+    # Remaining position arguments
+    parser.add_argument("args", nargs=argparse.REMAINDER)
+    # Parse the arguments
+    arguments = parser.parse_args()
+    arguments.level = arguments.level.lower()
+    logging_level_mapping = {
+        "debug": logging.DEBUG,
+        "info": logging.INFO,
+        "warning": logging.WARNING,
+        "error": logging.ERROR,
+        "critical": logging.CRITICAL}
+    if arguments.level in logging_level_mapping.keys():
+        level = logging_level_mapping[arguments.level]
+        logging.getLogger().setLevel(level)
 
     return arguments
 
