7d3c8d2b0eb7b7fa720304e51b34526b167245fd
Joeri Hermans <joeri@joerihermans.com>
2021-01-19 14:12:27 +0100

    Update the example with slurm decorators

--- hypothesis/workflow/__init__.py
+++ hypothesis/workflow/__init__.py
@@ -3,3 +3,6 @@ executor = None
 
 from .decorator import *
 from .util import *
+
+import .slurm
+import .local
--- hypothesis/workflow/slurm/__init__.py
+++ hypothesis/workflow/slurm/__init__.py
@@ -1,2 +1,3 @@
+from .decorator import *
 from .executor import execute
 from .util import slurm_detected
