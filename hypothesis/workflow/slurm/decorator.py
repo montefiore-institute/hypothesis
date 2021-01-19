fcaf2134294a4b440dbc2c7bb5582e2cbadd8296
Joeri Hermans <joeri@joerihermans.com>
2021-01-19 14:23:20 +0100

    Update the example with slurm decorators

--- hypothesis/workflow/slurm/decorator.py
+++ hypothesis/workflow/slurm/decorator.py
@@ -1,37 +1,35 @@
 import hypothesis.workflow as w
 
-from hypothesis.workflow import get_and_add_node
 
-
-@w.parameterize
+@w.parameterized
 def gpu(f, num_gpus):
     if num_gpus >= 1:
-        node = get_and_add_node(f)
+        node = w.get_and_add_node(f)
         node.set("--gres=gpu:", str(num_gpus))
 
     return f
 
-@w.parameterize
+@w.parameterized
 def name(f, name):
-    node = get_and_add_node(f)
+    node = w.get_and_add_node(f)
     node.name = name
     node.set("--job-name", str(name))
 
     return f
 
-@w.parameterize
+@w.parameterized
 def timelimit(f, time):
-    node = get_and_add_node(f)
+    node = w.get_and_add_node(f)
     node.set("--time", str(time))
 
     return f
 
-@w.parameterize
+@w.parameterized
 def memory(f, memory):
     r"""Specify the real memory required per node.
 
     Default units are megabytes. Different units can be
     specified using the suffix [K|M|G|T].
     """
-    node = get_and_add_node(f)
+    node = w.get_and_add_node(f)
     node.set("--mem", str(memory))
