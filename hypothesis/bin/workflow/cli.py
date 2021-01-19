09c9f364a1265212aa99dcb4174d766f18b5eed2
Joeri Hermans <joeri@joerihermans.com>
2021-01-19 02:00:00 +0100

    Fix optional modules for hypothesis cli

--- hypothesis/bin/workflow/cli.py
+++ hypothesis/bin/workflow/cli.py
@@ -10,6 +10,7 @@ import hypothesis.workflow.slurm
 import logging
 import os
 import sys
+import tempfile
 
 
 def main():
@@ -25,14 +26,23 @@ def main():
         "local": execute_local}
     # Check what module needs to be executed
     ## Execution
-    if len(arguments.execute) > 0:
+    if arguments.execute is not None:
         script = arguments.execute[1]
         exec(open(script).read(), globals())
         executors[executor](arguments)
+    elif arguments.list is not None:
+        list_store(arguments)
 
 
 def store_directory():
     return os.path.expanduser("~") + "/.hypothesis/workflow"
+
+
+def list_store(arguments):
+    paths = os.listdir(store_directory())
+    for p in paths:
+        if os.path.isdir(p):
+            print(p)
 
 
 def prepare_directory():
@@ -43,15 +53,17 @@ def prepare_directory():
 
 def execute_slurm(arguments):
     logging.info("Using the Slurm workflow backend.")
+    store = tempfile.mkdtemp(dir=store_directory())
+    hypothesis.workflow.slurm.execute(
+        directory=arguments.directory,
+        environment=arguments.environment,
+        store=store,
+        cleanup=not arguments.no_cleanup)
 
 
 def execute_local(arguments):
     logging.info("Using the local workflow backend.")
-    hypothesis.workflow.local.execute(
-        directory=arguments.directory,
-        environment=arguments.environment,
-        store=store_directory(),
-        cleanup=not arguments.no_cleanup)
+    hypothesis.workflow.local.execute()
 
 
 def load_default_executor():
@@ -73,11 +85,14 @@ def parse_arguments():
     parser.add_argument("--slurm", action="store_true", help="Force the usage the Slurm executor backend.")
     parser.add_argument("--local", action="store_true", help="Force the usage the local executor backend.")
     # Workflow modules
-    parser.add_argument("execute", nargs='+', help="Executes the specified workflow.")
-    parser.add_argument("status", nargs='+', help="Utility to monitor the status of workflows (only for the Slurm backend).")
-    parser.add_argument("cancel", nargs='+', help="Cancel the specified workflow (only for the Slurm backend).")
+    parser.add_argument("cancel", nargs='?', help="Cancel the specified workflow (only for the Slurm backend).")
+    parser.add_argument("clean", nargs='?', help="Clean up old workflows from the store (only for the Slurm backend).")
+    parser.add_argument("delete", nargs='?', help="Delete and cancel old or existing workflows (only for the Slurm backend).")
+    parser.add_argument("execute", nargs='?', help="Executes the specified workflow.")
+    parser.add_argument("list", nargs='?', help="Lists all workflows in the store (only for the Slurm backend).")
+    parser.add_argument("status", nargs='?', help="Utility to monitor the status of workflows (only for the Slurm backend).")
     # Parse the arguments
-    arguments = parser.parse_args()
+    arguments = parser.parse_intermixed_args()
     arguments.level = arguments.level.lower()
     logging_level_mapping = {
         "debug": logging.DEBUG,
