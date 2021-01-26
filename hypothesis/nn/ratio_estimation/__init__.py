3dad6dee29efe77a1db1c78f48b41668270c4c23
Joeri Hermans <joeri@joerihermans.com>
2021-01-26 22:49:55 +0100

    Add easy construction of ratio estimators

--- hypothesis/nn/ratio_estimation/__init__.py
+++ hypothesis/nn/ratio_estimation/__init__.py
@@ -2,4 +2,6 @@ from .base import BaseRatioEstimator
 from .base import RatioEstimatorEnsemble
 from .base import BaseCriterion
 
-from .util import build_ratio_estimator
+from .densenet import build_ratio_estimator as build_densenet_estimator
+from .mlp import build_ratio_estimator as build_mlp_estimator
+from .resnet import build_ratio_estimator as build_resnet_estimator
