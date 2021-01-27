a8e37de35d51afd633dcc896993ad3cadecfe7f3
Joeri Hermans <joeri@joerihermans.com>
2021-01-27 23:21:43 +0100

    Add likelihood ratio test statistic

--- hypothesis/stat/constraint.py
+++ hypothesis/stat/constraint.py
@@ -12,3 +12,13 @@ def likelihood_ratio_test_statistic(log_ratios):
     test_statistic -= test_statistic.min()
 
     return test_statistic
+
+
+@torch.no_grad()
+def confidence_level(log_ratios, dof=None, level=0.95):
+    if dof is None:
+        dof = log_ratios.dim() - 1
+    test_statistic = likelihood_ratio_test_statistic(log_ratios)
+    level = chi2.isf(1 - level, df=dof)
+
+    return test_statistic, level
