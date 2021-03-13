83d456746f1532f198aadb2b9ca86b93a14de771
Joeri Hermans <joeri@joerihermans.com>
2021-03-13 18:58:49 +0100

    Add plotting utilities

--- hypothesis/plot/util.py
+++ hypothesis/plot/util.py
@@ -1,3 +1,11 @@
-r"""
+r"""Plotting utilities for Hypothesis.
 
 """
+
+
+def make_square(ax):
+    r"""Makes the `matplotlib` axes square, irrespective of the data limits.
+
+    """
+    dr = ax.get_data_ratio()
+    ax.set_aspect(1.0 / dr, adjustable="box")
