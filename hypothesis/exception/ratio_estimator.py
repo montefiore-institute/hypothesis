8db8bd904384f8372a07e5efa8e04b878ee6a04d
Joeri Hermans <joeri@joerihermans.com>
2021-03-14 14:12:16 +0100

    Update

---
+++ hypothesis/exception/ratio_estimator.py
@@ -0,0 +1,30 @@
+r"""Exceptions related to ratio estimation.
+
+"""
+
+
+default_no_estimator_error = r"""Unknown ratio estimator specified.
+
+The following types are currently supported:
+ - `mlp`
+ - `resnet`
+ - `resnet-18`
+ - `resnet-34`
+ - `resnet-50`
+ - `resnet-101`
+ - `resnet-152`
+ - `densenet`
+ - `densenet-121`
+ - `densenet-161`
+ - `densenet-169`
+ - `densenet-201`
+"""
+
+class UnknownRatioEstimatorError(Exception):
+    r"""An exception to indicate the absence of a ratio estimator definition.
+
+    This happens whenever a ratio estimator identifier has been specified
+    which is not known to hypothesis.
+    """
+    def __init__(self, message=default_no_estimator_error):
+        super(NoWorkflowContextError, self).__init__(message)
