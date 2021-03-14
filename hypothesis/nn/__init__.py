209b768c90bc9b2c271a9e8c7efd8f20bc64da42
Joeri Hermans <joeri@joerihermans.com>
2021-03-14 14:36:21 +0100

    Update

--- hypothesis/nn/__init__.py
+++ hypothesis/nn/__init__.py
@@ -7,3 +7,5 @@ from .model import MLP
 from .model import ResNet
 
 import hypothesis.nn.ratio_estimation
+
+from hypothesis.nn.ratio_estimation import build_ratio_estimator
--- hypothesis/nn/ratio_estimation/__init__.py
+++ hypothesis/nn/ratio_estimation/__init__.py
@@ -6,3 +6,4 @@ from .base import ConservativeCriterion
 from .densenet import build_ratio_estimator as build_densenet_estimator
 from .mlp import build_ratio_estimator as build_mlp_estimator
 from .resnet import build_ratio_estimator as build_resnet_estimator
+from .util import build_ratio_estimator
