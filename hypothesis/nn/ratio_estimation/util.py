601719886ea820b98293cb2de4d3b48fad0b01c7
Joeri Hermans <joeri@joerihermans.com>
2021-03-14 14:30:49 +0100

    Add convenient ResNet ratio estimator allocation

--- hypothesis/nn/ratio_estimation/util.py
+++ hypothesis/nn/ratio_estimation/util.py
@@ -44,9 +44,43 @@ def build_mlp_ratio_estimator(random_variables, denominator, **kwargs):
     return build_ratio_estimator(random_variables, denominator, **kwargs)
 
 
+def build_resnet_18_ratio_estimator(random_variables, denominator, **kwargs):
+    r"""Utility method to easily allocate ResNet-18 ratio estimators."""
+    from hypothesis.nn.ratio_estimation.resnet import build_ratio_estimator
+    return build_ratio_estimator(random_variables, denominator, depth=18, **kwargs)
+
+
+def build_resnet_34_ratio_estimator(random_variables, denominator, **kwargs):
+    r"""Utility method to easily allocate ResNet-34 ratio estimators."""
+    from hypothesis.nn.ratio_estimation.resnet import build_ratio_estimator
+    return build_ratio_estimator(random_variables, denominator, depth=34, **kwargs)
+
+
+def build_resnet_50_ratio_estimator(random_variables, denominator, **kwargs):
+    r"""Utility method to easily allocate ResNet-50 ratio estimators."""
+    from hypothesis.nn.ratio_estimation.resnet import build_ratio_estimator
+    return build_ratio_estimator(random_variables, denominator, depth=50, **kwargs)
+
+
+def build_resnet_101_ratio_estimator(random_variables, denominator, **kwargs):
+    r"""Utility method to easily allocate ResNet-101 ratio estimators."""
+    from hypothesis.nn.ratio_estimation.resnet import build_ratio_estimator
+    return build_ratio_estimator(random_variables, denominator, depth=101, **kwargs)
+
+
+def build_resnet_152_ratio_estimator(random_variables, denominator, **kwargs):
+    r"""Utility method to easily allocate ResNet-152 ratio estimators."""
+    from hypothesis.nn.ratio_estimation.resnet import build_ratio_estimator
+    return build_ratio_estimator(random_variables, denominator, depth=152, **kwargs)
+
+
 _architectures = {
     # Multi-Layered Perceptron
     "mlp": build_mlp_ratio_estimator,
     # ResNet
     "resnet": build_resnet_18_ratio_estimator,
-    "resnet-18": build_resnet_18_ratio_estimator}
+    "resnet-18": build_resnet_18_ratio_estimator,
+    "resnet-34": build_resnet_34_ratio_estimator,
+    "resnet-50": build_resnet_50_ratio_estimator,
+    "resnet-101": build_resnet_101_ratio_estimator,
+    "resnet-152": build_resnet_152_ratio_estimator}
