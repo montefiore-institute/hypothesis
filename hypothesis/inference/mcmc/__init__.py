593e09b331b579a85982a09f386fcf81efdd6414
Joeri Hermans <joeri@joerihermans.com>
2021-04-02 09:13:06 +0200

    Rework MCMC

--- hypothesis/inference/mcmc/__init__.py
+++ hypothesis/inference/mcmc/__init__.py
@@ -6,3 +6,4 @@ import .proposal
 
 from .base import BaseMarkovChainMonteCarlo
 from .metropolis_hastings import MetropolisHastings
+from .aalr_metropolis_hastings import AALRMetropolisHastings
--- hypothesis/inference/mcmc/aalr_metropolis_hastings.py
+++ hypothesis/inference/mcmc/aalr_metropolis_hastings.py
@@ -1,28 +1,39 @@
-r"""MCMC methods based on Metropolis-Hastings.
+r"""Ammortized Approximate Likelihood Ratio Metropolis Hastings
 
+https://arxiv.org/abs/1903.04057
 """
 
+import hypothesis as h
 import numpy as np
 import torch
 
 from hypothesis.inference.mcmc import BaseMarkovChainMonteCarlo
 
 
-class MetropolisHastings(BaseMarkovChainMonteCarlo):
+class AALRMetropolisHastings(BaseMarkovChainMonteCarlo):
 
-    def __init__(self, prior, log_likelihood, proposal):
-        super(MetropolisHastings, self).__init__(prior)
+    def __init__(self, prior, ratio_estimator, proposal):
+        super(AALRMetropolisHastings, self).__init__(prior)
         self._denominator = None
-        self._log_likelihood = log_likelihood
+        self._r = ratio_estimator
         self._proposal = proposal
+
+    @torch.no_grad()
+    def _compute_ratio(self, input, outputs):
+        num_observations = outputs.shape[0]
+        inputs = input.repeat(num_observations, 1)
+        inputs = inputs.to(h.accelerator)
+        _, log_ratios = self.ratio_estimator(inputs=inputs, outputs=outputs)
+
+        return log_ratios.sum().cpu()
 
     @torch.no_grad()
     def _step(self, theta, observations):
         theta_next = self._proposal.sample(theta)
-        lnl_theta_next = self._log_likelihood(theta_next, observations)
+        lnl_theta_next = self._compute_ratio(theta_next, observations)
         numerator = self.prior.log_prob(theta_next) + lnl_theta_next
         if self._denominator is None:
-            lnl_theta = self._log_likelihood(theta, observations)
+            lnl_theta = self._compute_ratio(theta, observations)
             self._denominator = self.prior.log_prob(theta) + lnl_theta_next
         acceptance_ratio = numerator - self._denominator
         if not self._proposal.is_symmetrical():
