02bda54d7c059ab3a276621d532dde1ef551a003
Joeri Hermans <joeri@joerihermans.com>
2021-04-07 11:48:00 +0200

    Move prior to GPU when available

--- hypothesis/benchmark/weinberg/util.py
+++ hypothesis/benchmark/weinberg/util.py
@@ -2,13 +2,18 @@ r"""Utilities for the Weinberg benchmark.
 
 """
 
+import hypothesis as h
 import torch
 
 
 @torch.no_grad()
 def Prior():
     r"""Returns a prior ``Uniform(0.25, 2.0)`` over the Fermi constant."""
-    return torch.distributions.uniform.Uniform(0.25, 2.0)
+    lower = torch.tensor(0.25).float()
+    upper = torch.tensor(2.0).float()
+    lower = lower.to(h.accelerator)
+    upper = upper.to(h.accelerator)
+    return torch.distributions.uniform.Uniform(lower, upper)
 
 
 @torch.no_grad()
@@ -17,10 +22,14 @@ def PriorExperiment():
     the experimental design space and represents the beam energy in GeV.
 
     """
-    return torch.distributions.uniform.Uniform(40.0, 50.0)  # KeV
+    lower = torch.tensor(40.0).float()
+    upper = torch.tensor(50.0).float()
+    lower = lower.to(h.accelerator)
+    upper = upper.to(h.accelerator)
+    return torch.distributions.uniform.Uniform(lower, upper)  # KeV
 
 
 @torch.no_grad()
 def Truth():
     r"""Returns the true Fermi constant for this benchmark problem: ``1.0``."""
-    return torch.tensor(1.0)
+    return torch.tensor(1.0).float()
