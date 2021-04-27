d39fd14207bd4d9af7d8ef2a9d95d44b318e2ce8
Joeri Hermans <joeri@joerihermans.com>
2021-04-27 07:50:15 +0200

    Remove old flow optimizer

--- hypothesis/train/__init__.py
+++ hypothesis/train/__init__.py
@@ -4,4 +4,3 @@ r"""Base definition of optimization procedures and their utilities.
 
 from .base import BaseTrainer
 from .ratio_estimation import RatioEstimatorTrainer
-from .flow_posterior_estimation import FlowPosteriorEstimatorTrainer
